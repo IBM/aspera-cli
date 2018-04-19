8313bbfc5b25ffa89c31ab12855ec9ea6a505f2e
Laurent Martin <laurent@asperasoft.com>
2018-04-19 17:13:47 +0200

    wf id is now an option instead of argument

--- lib/asperalm/cli/plugins/orchestrator.rb
+++ lib/asperalm/cli/plugins/orchestrator.rb
@@ -8,11 +8,11 @@ module Asperalm
       class Orchestrator < BasicAuthPlugin
 
         alias super_declare_options declare_options
-
         def declare_options
           super_declare_options
           Main.tool.options.add_opt_simple(:params,"parameters hash table, use @json:{\"param\":\"value\"}")
           Main.tool.options.add_opt_simple(:result,"specify result value as: 'work step:parameter'")
+          Main.tool.options.add_opt_simple(:id,"workflow identifier")
           Main.tool.options.add_opt_boolean(:synchronous,"work step:parameter expected as result")
           Main.tool.options.set_option(:params,{})
           Main.tool.options.set_option(:synchronous,:no)
@@ -69,7 +69,10 @@ module Asperalm
             result=call_API("api/plugin_version")[:data]
             return {:type=>:hash_array,:data=>result['Plugin']}
           when :workflow
-            command=Main.tool.options.get_next_argument('command',[:list, :status, :id])
+            command=Main.tool.options.get_next_argument('command',[:list, :status, :inputs, :details, :start])
+            unless [:list, :status].include?(command)
+              wf_id=Main.tool.options.get_option(:id,:mandatory)
+            end
             case command
             when :status
               result=call_API("api/workflows_status")[:data]
@@ -77,45 +80,40 @@ module Asperalm
             when :list
               result=call_API("workflow_reporter/workflows_list/0")[:data]
               return {:type=>:hash_array,:data=>result['workflows']['workflow'],:fields=>["id","portable_id","name","published_status","published_revision_id","latest_revision_id","last_modification"]}
-            when :id
-              wf_id=Main.tool.options.get_next_argument('workflow id')
-              command=Main.tool.options.get_next_argument('command',[:inputs,:status,:start])
-              case command
-              when :status
-                result=call_API("api/workflow_details",wf_id)[:data]
-                return {:type=>:hash_array,:data=>result['workflows']['workflow']['statuses']}
-              when :inputs
-                result=call_API("api/workflow_inputs_spec",wf_id)[:data]
-                return {:type=>:key_val_list,:data=>result['workflow_inputs_spec']}
-              when :start
-                result={
-                  :type=>:key_val_list,
-                  :data=>nil
-                }
-                call_params={}
-                # set external parameters if any
-                Main.tool.options.get_option(:params,:mandatory).each do |name,value|
-                  call_params["external_parameters[#{name}]"] = value
-                end
-                # synchronous call ?
-                call_params["synchronous"]=true if Main.tool.options.get_option(:synchronous,:mandatory)
-                # expected result for synchro call ?
-                expected=Main.tool.options.get_option(:result,:optional)
-                if !expected.nil?
-                  result[:type] = :status
-                  fields=expected.split(/:/)
-                  raise "Expects: work_step:result_name format, but got #{expected}" if fields.length != 2
-                  call_params["explicit_output_step"]=fields[0]
-                  call_params["explicit_output_variable"]=fields[1]
-                  # implicitely, call is synchronous
-                  call_params["synchronous"]=true
-                end
-                result[:data]=call_API("api/initiate",wf_id,call_params)[:data]
-                return result
+            when :details
+              result=call_API("api/workflow_details",wf_id)[:data]
+              return {:type=>:hash_array,:data=>result['workflows']['workflow']['statuses']}
+            when :inputs
+              result=call_API("api/workflow_inputs_spec",wf_id)[:data]
+              return {:type=>:key_val_list,:data=>result['workflow_inputs_spec']}
+            when :start
+              result={
+                :type=>:key_val_list,
+                :data=>nil
+              }
+              call_params={}
+              # set external parameters if any
+              Main.tool.options.get_option(:params,:mandatory).each do |name,value|
+                call_params["external_parameters[#{name}]"] = value
               end
-            else
-              raise "ERROR, unknown command: [#{command}]"
+              # synchronous call ?
+              call_params["synchronous"]=true if Main.tool.options.get_option(:synchronous,:mandatory)
+              # expected result for synchro call ?
+              expected=Main.tool.options.get_option(:result,:optional)
+              if !expected.nil?
+                result[:type] = :status
+                fields=expected.split(/:/)
+                raise "Expects: work_step:result_name format, but got #{expected}" if fields.length != 2
+                call_params["explicit_output_step"]=fields[0]
+                call_params["explicit_output_variable"]=fields[1]
+                # implicitely, call is synchronous
+                call_params["synchronous"]=true
+              end
+              result[:data]=call_API("api/initiate",wf_id,call_params)[:data]
+              return result
             end
+          else
+            raise "ERROR, unknown command: [#{command}]"
           end
         end # execute_action
       end # Orchestrator
