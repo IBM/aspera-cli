e805e2523862e979382589590c2d3ed381cd044a
Laurent Martin <laurent@asperasoft.com>
2018-04-23 15:13:02 +0200

    support local commands

--- lib/asperalm/cli/plugins/server.rb
+++ lib/asperalm/cli/plugins/server.rb
@@ -7,6 +7,12 @@ module Asperalm
     module Plugins
       # implement basic remote access with FASP/SSH
       class Server < BasicAuthPlugin
+        class LocalExecutor
+          def execute(cmd,input=nil)
+            `#{cmd}`
+          end
+        end
+
         def initialize
         end
 
@@ -23,34 +29,39 @@ module Asperalm
         def execute_action
           server_uri=URI.parse(@optmgr.get_option(:url,:mandatory))
           Log.log.debug("URI : #{server_uri}, port=#{server_uri.port}, scheme:#{server_uri.scheme}")
-          raise CliError,"Only ssh scheme is supported in url" if !server_uri.scheme.eql?("ssh")
-
-          transfer_spec={
-            "remote_host"=>server_uri.hostname,
-            "remote_user"=>@optmgr.get_option(:username,:mandatory),
-          }
-          ssh_options={}
-          if !server_uri.port.nil?
-            ssh_options[:port]=server_uri.port
-            transfer_spec["ssh_port"]=server_uri.port
+          shell_executor=nil
+          case server_uri.scheme
+          when 'ssh'
+            transfer_spec={
+              "remote_host"=>server_uri.hostname,
+              "remote_user"=>@optmgr.get_option(:username,:mandatory),
+            }
+            ssh_options={}
+            if !server_uri.port.nil?
+              ssh_options[:port]=server_uri.port
+              transfer_spec["ssh_port"]=server_uri.port
+            end
+            cred_set=false
+            password=@optmgr.get_option(:password,:optional)
+            if !password.nil?
+              ssh_options[:password]=password
+              transfer_spec['remote_password']=password
+              cred_set=true
+            end
+            ssh_keys=@optmgr.get_option(:ssh_keys,:optional)
+            raise "internal error, expecting array" if !ssh_keys.is_a?(Array)
+            if !ssh_keys.empty?
+              ssh_options[:keys]=ssh_keys
+              transfer_spec["EX_ssh_key_paths"]=ssh_keys
+              cred_set=true
+            end
+            raise "either password or key must be provided" if !cred_set
+            shell_executor=Ssh.new(transfer_spec["remote_host"],transfer_spec["remote_user"],ssh_options)
+          when 'local'
+            shell_executor=LocalExecutor.new
+          else
+            raise CliError,"Only ssh scheme is supported in url" if !server_uri.scheme.eql?("ssh")
           end
-          cred_set=false
-          password=@optmgr.get_option(:password,:optional)
-          if !password.nil?
-            ssh_options[:password]=password
-            transfer_spec['remote_password']=password
-            cred_set=true
-          end
-          ssh_keys=@optmgr.get_option(:ssh_keys,:optional)
-          raise "internal error, expecting array" if !ssh_keys.is_a?(Array)
-          if !ssh_keys.empty?
-            ssh_options[:keys]=ssh_keys
-            transfer_spec["EX_ssh_key_paths"]=ssh_keys
-            cred_set=true
-          end
-          raise "either password or key must be provided" if !cred_set
-          ssh_executor=Ssh.new(transfer_spec["remote_host"],transfer_spec["remote_user"],ssh_options)
-          ascmd=Asperalm::AsCmd.new(ssh_executor)
 
           # get command and set aliases
           command=@optmgr.get_next_argument('command',action_list)
@@ -63,8 +74,33 @@ module Asperalm
               realcmd="as"+command.to_s
               args = @optmgr.get_next_argument("#{realcmd} arguments",:multiple)
               # concatenate arguments, enclose in double quotes
-              command = args.unshift(realcmd).map{|v|'"'+v+'"'}.join(" ")
-              return Plugin.result_status(ssh_executor.execute(command))
+              command_line = args.unshift(realcmd).map{|v|'"'+v+'"'}.join(" ")
+              result=shell_executor.execute(command_line)
+              if command.eql?(:configurator)
+                lines=result.split("\n")
+                Log.log.debug(`type asconfigurator`)
+                result=lines
+                if lines.first.eql?('success')
+                  lines.shift
+                  result={}
+                  lines.each do |line|
+                    Log.log.debug("#{line}")
+                    data=line.split(',').map{|i|i.gsub(/^"/,'').gsub(/"$/,'')}.map{|i|case i;when'AS_NULL';nil;when'true';true;when'false';false;else i;end}
+                    Log.log.debug("#{data}")
+                    section=data.shift
+                    datapart=result[section]||={}
+                    if section.eql?('user')
+                      name=data.shift
+                      datapart=datapart[name]||={}
+                    end
+                    datapart=datapart[data.shift]={}
+                    datapart['default']=data.pop
+                    datapart['value']=data.pop
+                  end
+                  return {:type=>:key_val_list,:data=>result,:fields=>['section','name','value','default'],:option_expand_last=>true}
+                end
+              end
+              return Plugin.result_status(result)
             when :upload
               filelist = @optmgr.get_next_argument("source list",:multiple)
               transfer_spec.merge!({
@@ -81,6 +117,7 @@ module Asperalm
               return @main.start_transfer(transfer_spec)
             when *Asperalm::AsCmd.action_list
               args=@optmgr.get_next_argument('ascmd command arguments',:multiple,:optional)
+              ascmd=Asperalm::AsCmd.new(shell_executor)
               result=ascmd.send(:execute_single,command,args)
               case command
               when :mkdir; return Plugin.result_success
