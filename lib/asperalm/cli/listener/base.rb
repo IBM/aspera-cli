4c2faaacc757f2d1bd72a45d4cb40d8c65e9927c
Laurent Martin <laurent@asperasoft.com>
2018-09-28 13:55:10 +0200

    moved to fasp module

--- lib/asperalm/cli/listener/base.rb
+++ lib/asperalm/cli/listener/base.rb
@@ -1,13 +1,11 @@
 module Asperalm
-  module Cli
-    module Listener
-      # imlement this class to get transfer events
-      class Base
-        # define one of the following methods:
-        # event_text(text_data)
-        # event_struct(legacy_names)
-        # event_enhanced(snake_names)
-      end
+  module Fasp
+    # imlement this class to get transfer events
+    class Listener
+      # define one of the following methods:
+      # event_text(text_data)
+      # event_struct(legacy_names)
+      # event_enhanced(snake_names)
     end
   end
 end
