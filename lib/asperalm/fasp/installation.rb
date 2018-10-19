e98d0c391e8a33601660421ff1f1bc117bc0d57e
Laurent Martin <laurent@asperasoft.com>
2018-10-19 09:34:11 +0200

    added driove for mac

--- lib/asperalm/fasp/installation.rb
+++ lib/asperalm/fasp/installation.rb
@@ -66,6 +66,7 @@ module Asperalm
         if @found_products.nil?
           @found_products=product_locations.select do |l|
             next false unless Dir.exist?(l[:app_root])
+            Log.log.debug("found #{l[:app_root]}")
             product_info_file="#{l[:app_root]}/#{PRODUCT_INFO}"
             if File.exist?(product_info_file)
               res_s=XmlSimple.xml_in(File.read(product_info_file),{"ForceArray"=>false})
@@ -86,14 +87,18 @@ module Asperalm
         return File.join(connect[:run_root],VARRUN_SUBFOLDER,'https.uri')
       end
 
+      def cli_conf_file
+        connect=get_product('Aspera CLI')
+        return File.join(connect[:app_root],BIN_SUBFOLDER,'.aspera_cli_conf')
+      end
       private
 
       def get_product(name)
-        found=installed_products.select{|i|i[:name].eql?(name)}
+        found=installed_products.select{|i|i[:expected].eql?(name) or i[:name].eql?(name)}
         raise "Product: #{name} not found, please install." if found.empty?
         return found.first
       end
-      
+
       def initialize
         @selected_product_paths=nil
         @found_products=nil
@@ -112,7 +117,7 @@ module Asperalm
       #        :dsa=>'aspera_tokenauth_id_dsa'}
       def self.get_product_paths(p)
         exec_ext = OpenApplication.current_os_type.eql?(:windows) ? '.exe' : ''
-        # default values
+        # set default values if specific ones not specified
         sub_bin = p[:sub_bin] || BIN_SUBFOLDER
         sub_keys = p[:sub_keys] || ETC_SUBFOLDER
         result={
@@ -128,8 +133,8 @@ module Asperalm
         # server software (having asperanoded) has a different DSA filename
         server_dsa=File.join(p[:app_root],sub_keys,SERVER_DSA)
         result[:ssh_bypass_key_dsa][:path]=server_dsa  if File.exist?(server_dsa)
-
         Log.log.debug "resources=#{result}"
+        # check required files
         notfound=[]
         result.each_pair do |k,v|
           notfound.push(k) if v[:type].eql?(:file) and v[:required] and ! File.exist?(v[:path])
@@ -145,10 +150,10 @@ module Asperalm
       # fields
       # :expected  M app name is taken from the manifest if present, else defaults to this value
       # :app_root  M main forlder for the application
-      # :sub_bin   O subfolder with executables, default : bin
-      # :sub_keys  O subfolder with keys, default : etc
       # :log_root  O location of log files (Linux uses syslog)
       # :run_root  O only for Connect Client, location of http port file
+      # :sub_bin   O subfolder with executables, default : bin
+      # :sub_keys  O subfolder with keys, default : etc
       def product_locations
         case OpenApplication.current_os_type
         when :windows; return [{
@@ -158,33 +163,39 @@ module Asperalm
             :run_root =>File.join(ENV['LOCALAPPDATA'],'Aspera','Aspera Connect')
             },{
             :expected =>'Aspera CLI',
-            :app_root =>'C:\Program Files\Aspera\cli',
-            :sub_keys =>'var',
-            :log_root =>'C:\Program Files\Aspera\cli\var\log'
+            :app_root =>File.join('C:','Program Files','Aspera','cli'),
+            :log_root =>File.join('C:','Program Files','Aspera','cli','var','log'),
+            :sub_keys =>'var'
             },{
             :expected =>'Enterprise Server',
-            :app_root =>'C:\Program Files\Aspera\Enterprise Server',
-            :sub_keys =>'var',
-            :log_root =>'C:\Program Files\Aspera\Enterprise Server\var\log'
+            :app_root =>File.join('C:','Program Files','Aspera','Enterprise Server'),
+            :log_root =>File.join('C:','Program Files','Aspera','Enterprise Server','var','log'),
+            :sub_keys =>'var'
             }]
         when :mac; return [{
             :expected =>'Aspera Connect',
             :app_root =>File.join(Dir.home,'Applications','Aspera Connect.app'),
-            :sub_bin  =>File.join('Contents','Resources'),
-            :sub_keys =>File.join('Contents','Resources'),
             :log_root =>File.join(Dir.home,'Library','Logs','Aspera_Connect'),
-            :run_root =>File.join(Dir.home,'Library','Application Support','Aspera','Aspera Connect')
+            :run_root =>File.join(Dir.home,'Library','Application Support','Aspera','Aspera Connect'),
+            :sub_bin  =>File.join('Contents','Resources'),
+            :sub_keys =>File.join('Contents','Resources')
             },{
             :expected =>'Aspera CLI',
             :app_root =>File.join(Dir.home,'Applications','Aspera CLI'),
             :log_root =>File.join(Dir.home,'Library','Logs','Aspera')
             },{
             :expected =>'Enterprise Server',
-            :app_root =>'/Library/Aspera',
-            :sub_keys =>'var',
-            :log_root =>File.join(Dir.home,'Library','Logs','Aspera')
+            :app_root =>File.join('','Library','Aspera'),
+            :log_root =>File.join(Dir.home,'Library','Logs','Aspera'),
+            :sub_keys =>'var'
+            },{
+            :expected =>'Aspera Drive',
+            :app_root =>File.join('','Applications','Aspera Drive.app'),
+            :log_root =>File.join(Dir.home,'Library','Logs','Aspera_Drive'),
+            :sub_bin  =>File.join('Contents','Resources'),
+            :sub_keys =>File.join('Contents','Resources')
             }]
-        else; return [{  # other: unix family
+        else; return [{  # other: Linux and unix family
             :expected =>'Aspera Connect',
             :app_root =>File.join(Dir.home,'.aspera','connect'),
             :run_root =>File.join(Dir.home,'.aspera','connect')
@@ -193,7 +204,7 @@ module Asperalm
             :app_root =>File.join(Dir.home,'.aspera','cli'),
             },{
             :expected =>'Enterprise Server',
-            :app_root =>'/opt/aspera',
+            :app_root =>File.join('','opt','aspera'),
             :sub_keys =>'var'
             }]
         end
