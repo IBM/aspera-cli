ac721e844f8bb8a21ea2e4d5e660926f54d894eb
Laurent Martin <laurent@asperasoft.com>
2018-12-05 01:05:54 +0100

    refactor

--- lib/asperalm/colors.rb
+++ lib/asperalm/colors.rb
@@ -1,11 +1,11 @@
 # simple vt100 colors
 class String
   private
-  def self.vtcmd(code); "\e[#{code}m";end
-
+  def self.vtcmd(code);"\e[#{code}m";end
   # see https://en.wikipedia.org/wiki/ANSI_escape_code
-  # here the symbol is the method name added to String to add control chars to set color (and reset at the end).
-  VTSTYLES = {
+  # symbol is the method name added to String
+  # it adds control chars to set color (and reset at the end).
+  @@VTSTYLES = {
     :bold=>1,
     :italic=>3,
     :underline=>4,
@@ -28,12 +28,11 @@ class String
     :bg_cyan=>46,
     :bg_gray=>47,
   }
-  public
-  # defines methods to String, one per entry in VTSTYLES
-  VTSTYLES.each do |name,code|
-    # the end code depends on value
-    endcode = (code >= 10) ? 0 : code+20+(code.eql?(1)?1:0)
-    eval "def #{name}; \"#{vtcmd(code)}\#{self}#{vtcmd(endcode)}\"; end"
+  # defines methods to String, one per entry in @@VTSTYLES
+  @@VTSTYLES.each do |name,code|
+    begin_seq=vtcmd(code)
+    end_seq=vtcmd((code >= 10) ? 0 : code+20+(code.eql?(1)?1:0))
+    define_method(name){"#{begin_seq}#{self}#{end_seq}"}
+    public name
   end
-
 end
