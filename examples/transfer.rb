33446d0bb8f302dae6f97e081605a5f06e6b2112
Laurent Martin <laurent@asperasoft.com>
2018-12-18 11:47:06 +0100

    more examples

--- examples/transfer.rb
+++ examples/transfer.rb
@@ -1,8 +1,24 @@
+# Example: transfer a file using one of the provided transfer agents
 $LOAD_PATH.unshift(File.dirname(__FILE__)+"/../lib")
 require 'asperalm/fasp/local'
+require 'asperalm/fasp/connect'
+require 'asperalm/fasp/node'
 require 'asperalm/fasp/listener'
 require 'asperalm/fasp/installation'
+require 'asperalm/log'
+require 'asperalm/rest'
 require 'json'
+class MyListener < Asperalm::Fasp::Listener
+  def event_enhanced(data);STDOUT.puts(JSON.generate(data));STDOUT.flush;end
+end
+Asperalm::Log.instance.level=:debug
+Asperalm::Fasp::Installation.instance.installed_products.each{|p|puts("found: #{p[:name]}")}
+Asperalm::Fasp::Installation.instance.activated='Aspera Connect'
+fasp_manager=Asperalm::Fasp::Local.instance
+fasp_manager=Asperalm::Fasp::Connect.instance
+fasp_manager=Asperalm::Fasp::Node.instance
+Asperalm::Fasp::Node.instance.node_api=Asperalm::Rest.new()
+fasp_manager.add_listener(MyListener.new)
 transfer_spec={
   'remote_host'     =>'demo.asperasoft.com',
   'remote_user'     =>'asperaweb',
@@ -11,14 +27,5 @@ transfer_spec={
   'destination_root'=>'.',
   'paths'           =>[{'source'=>'aspera-test-dir-tiny/200KB.1'}]
 }
-class MyListener < Asperalm::Fasp::Listener
-  def event_enhanced(data)
-    STDOUT.puts(JSON.generate(data));STDOUT.flush
-  end
-end
-Asperalm::Fasp::Installation.instance.installed_products.each{|p|puts("found: #{p[:name]}")}
-Asperalm::Fasp::Installation.instance.activated='Aspera Connect'
-fasp_manager=Asperalm::Fasp::Local.instance
-fasp_manager.add_listener(MyListener.new)
 fasp_manager.start_transfer(transfer_spec)
 fasp_manager.wait_for_transfers_completion
