457bd36ef6f8ecae3951a8f069d3f98c6dcb3df7
Laurent Martin <laurent@asperasoft.com>
2018-12-18 11:10:51 +0100

    new sample

---
+++ examples/transfer.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)+"/../lib")
+require 'asperalm/fasp/local'
+require 'asperalm/fasp/listener'
+require 'asperalm/fasp/installation'
+require 'json'
+transfer_spec={
+  'remote_host'     =>'demo.asperasoft.com',
+  'remote_user'     =>'asperaweb',
+  'remote_password' =>'demoaspera',
+  'direction'       =>'receive',
+  'destination_root'=>'.',
+  'paths'           =>[{'source'=>'aspera-test-dir-tiny/200KB.1'}]
+}
+class MyListener < Asperalm::Fasp::Listener
+  def event_enhanced(data)
+    STDOUT.puts(JSON.generate(data));STDOUT.flush
+  end
+end
+Asperalm::Fasp::Installation.instance.installed_products.each{|p|puts("found: #{p[:name]}")}
+Asperalm::Fasp::Installation.instance.activated='Aspera Connect'
+fasp_manager=Asperalm::Fasp::Local.instance
+fasp_manager.add_listener(MyListener.new)
+fasp_manager.start_transfer(transfer_spec)
+fasp_manager.wait_for_transfers_completion
