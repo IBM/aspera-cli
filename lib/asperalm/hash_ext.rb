b76a72994c491a6d9b998a21c2356cc3b38d579b
Laurent Martin <laurent@asperasoft.com>
2019-01-07 22:06:10 +0100

    moved hash extended interface

---
+++ lib/asperalm/hash_ext.rb
@@ -0,0 +1,16 @@
+# for older rubies
+unless Hash.method_defined?(:dig)
+  class Hash
+    def dig(*path)
+      path.inject(self) do |location, key|
+        location.respond_to?(:keys) ? location[key] : nil
+      end
+    end
+  end
+end
+
+class Hash
+  def deep_merge(second)
+    self.merge(second){|key,v1,v2|Hash===v1&&Hash===v2 ? v1.deep_merge(v2) : v2}
+  end
+end
