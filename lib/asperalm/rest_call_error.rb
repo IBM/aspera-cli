8cb777bbc87d3ac42aca3fa92a297719ae11c7dc
Laurent Martin <laurent@asperasoft.com>
2019-03-04 12:05:43 +0100

    separate error handlers

--- lib/asperalm/rest_call_error.rb
+++ lib/asperalm/rest_call_error.rb
@@ -15,66 +15,82 @@ module Asperalm
       super("#{@response.code} #{@request['host']}: #{msg}")
     end
 
-    # called by the Rest object on any result
-    def self.raiseOnError(req,resp)
-      data=JSON.parse(resp.body) rescue nil
-      # set to non-nil if there is an error
-      msg=nil
-      # see if there is a more precise message
-      # Hum, we would need some consistency here... payload analyzer
-      # we have a payload
-      if data.is_a?(Hash)
-        # Type 1
-        d_error=data['error']
-        if d_error.is_a?(Hash)
-          d_error=data['error']
-          if d_error["user_message"].is_a?(String)
-            msg=d_error["user_message"]
-          elsif d_error["description"].is_a?(String)
-            msg=d_error["description"]
-          end
-          # Faspex
-          if d_error['internal_message'].is_a?(String)
-            msg=msg+": "+d_error['internal_message']
-          end
-        elsif data['error'].is_a?(String)
-          msg=data['error']
+    # handlers should probably be defined by plugins
+    ERROR_HANDLERS=[]
+
+    def self.add_handler(err_type,&block)
+      ERROR_HANDLERS.push(block)
+    end
+    add_handler("Type 1.1") do |msg_stack,json_response,req,resp|
+      d_error=json_response['error']
+      k='user_message'
+      msg_stack.push(d_error[k]) if d_error.is_a?(Hash) and d_error[k].is_a?(String)
+    end
+    add_handler("Type 1.2") do |msg_stack,json_response,req,resp|
+      d_error=json_response['error']
+      k='description'
+      msg_stack.push(d_error[k]) if d_error.is_a?(Hash) and d_error[k].is_a?(String)
+    end
+    add_handler("Type 1.3") do |msg_stack,json_response,req,resp|
+      d_error=json_response['error']
+      k='internal_message'
+      msg_stack.push(d_error[k]) if d_error.is_a?(Hash) and d_error[k].is_a?(String)
+    end
+    add_handler("Type 1.4") do |msg_stack,json_response,req,resp|
+      d_error=json_response['error']
+      msg_stack.push(d_error) if d_error.is_a?(String)
+    end
+    add_handler("Type 2") do |msg_stack,json_response,req,resp|
+      # Type 2
+      # TODO: json_response['code'] and json_response['message'] ?
+      msg_stack.push(json_response['error_description']) if json_response['error_description'].is_a?(String)
+    end
+    add_handler("Type 3") do |msg_stack,json_response,req,resp|
+      # Type 3
+      if json_response['message'].is_a?(String)
+        msg_stack.push(json_response['message'])
+        # add other fields as info
+        json_response.each do |k,v|
+          msg_stack.push("#{k}: #{v}") unless k.eql?('message')
         end
-        # Type 2
-        # TODO: data['code'] and data['message'] ?
-        if data['error_description'].is_a?(String)
-          msg=msg+": "+data['error_description']
+      end
+    end
+    add_handler("Type 4") do |msg_stack,json_response,req,resp|
+      if json_response['errors'].is_a?(Hash)
+        json_response['errors'].each do |k,v|
+          msg_stack.push("#{k}: #{v}")
         end
-        # Type 3
-        if data['message'].is_a?(String)
-          msg=msg+": "+data['message']
-          data.delete('message')
-          # ???
-          data.each do |k,v|
-            msg=msg+"\n#{k}: #{v}"
-          end
-        end
-        # Type 4
-        if data['errors'].is_a?(Hash)
-          data['errors'].each do |k,v|
-            msg=msg+"\n#{k}: #{v}"
-          end
-        end
-        # Type 5 : call to upload_setup and download_setup of node api
-        d_t_s=data['transfer_specs']
-        if d_t_s.is_a?(Array)
-          d_t_s.each do |res|
-            r_err=res['transfer_spec']['error']
-            if r_err.is_a?(Hash)
-              msg||=''
-              msg="#{msg}\n#{r_err['code']}: #{r_err['reason']}: #{r_err['user_message']}"
-            end
+      end
+    end
+    # call to upload_setup and download_setup of node api
+    add_handler("Type 5") do |msg_stack,json_response,req,resp|
+      d_t_s=json_response['transfer_specs']
+      if d_t_s.is_a?(Array)
+        d_t_s.each do |res|
+          r_err=res['transfer_spec']['error']
+          if r_err.is_a?(Hash)
+            msg_stack.push("#{r_err['code']}: #{r_err['reason']}: #{r_err['user_message']}")
           end
         end
       end
-      unless resp.code.start_with?('2') and msg.nil?
-        msg||=resp.message
-        raise RestCallError.new(req,resp,msg)
+    end
+
+    # called by the Rest object on any result
+    def self.raiseOnError(req,resp)
+      # get error messages if any
+      msg_stack=[]
+      begin
+        json_response=JSON.parse(resp.body)
+        # handlers called only if valid JSON found
+        ERROR_HANDLERS.each do |handler|
+          handler.call(msg_stack,json_response,req,resp)
+        end
+      rescue
+        nil
+      end
+      unless resp.code.start_with?('2') and msg_stack.empty?
+        msg_stack.push(resp.message) if msg_stack.empty?
+        raise RestCallError.new(req,resp,msg_stack.join("\n"))
       end
     end
   end
