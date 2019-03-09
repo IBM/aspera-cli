0213abcec6d198a3b98548c561af1e6742cd36bb
Laurent Martin <laurent@asperasoft.com>
2019-03-09 18:34:57 +0100

    refactor

--- lib/asperalm/command_line_builder.rb
+++ lib/asperalm/command_line_builder.rb
@@ -7,12 +7,12 @@ module Asperalm
 
     private
     # default value for command line based on option name
-    def switch_name(ts_name,options)
+    def switch_name(param_name,options)
       return options[:option_switch] if options.has_key?(:option_switch)
-      return '--'+ts_name.to_s.gsub('_','-')
+      return '--'+param_name.to_s.gsub('_','-')
     end
 
-    def env_name(ts_name,options)
+    def env_name(param_name,options)
       return options[:variable]
     end
 
@@ -20,21 +20,27 @@ module Asperalm
 
     BOOLEAN_CLASSES=[TrueClass,FalseClass]
 
-    # @param job_params
-    def initialize(job_params,params_definition)
-      @job_params=job_params.clone # shallow copy is sufficient
+    # @param param_hash
+    def initialize(param_hash,params_definition)
+      @param_hash=param_hash.clone # shallow copy is sufficient
+      @params_definition=params_definition
       @result_env={}
       @result_args=[]
-      @used_ts_keys=[]
-      @params_definition=params_definition
+      @used_param_names=[]
+    end
+    
+    def warn_unrecognized_params
+      # warn about non translated arguments
+      @param_hash.each_pair{|key,val|Log.log.error("unrecognized parameter: #{key} = \"#{val}\"") if !@used_param_names.include?(key)}
     end
 
-    def add_env_args(env_args)
-      # warn about non translated arguments
-      @job_params.each_pair{|key,val|Log.log.error("unrecognized parameter: #{key} = \"#{val}\"") if !@used_ts_keys.include?(key)}
+    # adds keys :env :args with resulting values after processing
+    # warns if some parameters were not used
+    def add_env_args(env,args)
       Log.log.debug("ENV=#{@result_env}, ARGS=#{@result_args}")
-      env_args[:env].merge!(@result_env)
-      env_args[:args].push(*@result_args)
+      warn_unrecognized_params
+      env.merge!(@result_env)
+      args.push(*@result_args)
       return nil
     end
 
@@ -60,10 +66,10 @@ module Asperalm
     end
 
     # Process a parameter from transfer specification and generate command line param or env var
-    # @param ts_name : key in transfer spec
+    # @param param_name : key in transfer spec
     # @param option_type : type of processing
     # @param options : options for type
-    def process_param(ts_name,option_type,options={})
+    def process_param(param_name,option_type,options={})
       # by default : not mandatory
       options[:mandatory]||=false
       if options.has_key?(:accepted_types)
@@ -74,12 +80,12 @@ module Asperalm
         options[:accepted_types]=option_type.eql?(:opt_without_arg) ? BOOLEAN_CLASSES : [String]
       end
       # check mandatory parameter (nil is valid value)
-      raise Fasp::Error.new("mandatory parameter: #{ts_name}") if options[:mandatory] and !@job_params.has_key?(ts_name)
-      parameter_value=@job_params[ts_name]
+      raise Fasp::Error.new("mandatory parameter: #{param_name}") if options[:mandatory] and !@param_hash.has_key?(param_name)
+      parameter_value=@param_hash[param_name]
       parameter_value=options[:default] if parameter_value.nil? and options.has_key?(:default)
       # check provided type
-      raise Fasp::Error.new("#{ts_name} is : #{parameter_value.class} (#{parameter_value}), shall be #{options[:accepted_types]}, ") unless parameter_value.nil? or options[:accepted_types].inject(false){|m,v|m or parameter_value.is_a?(v)}
-      @used_ts_keys.push(ts_name)
+      raise Fasp::Error.new("#{param_name} is : #{parameter_value.class} (#{parameter_value}), shall be #{options[:accepted_types]}, ") unless parameter_value.nil? or options[:accepted_types].inject(false){|m,v|m or parameter_value.is_a?(v)}
+      @used_param_names.push(param_name)
 
       # process only non-nil values
       return nil if parameter_value.nil?
@@ -93,7 +99,7 @@ module Asperalm
       raise "unsupported value: #{parameter_value}" unless options[:accepted_values].nil? or options[:accepted_values].include?(parameter_value)
       if options[:encode]
         newvalue=options[:encode].call(parameter_value)
-        raise Fasp::Error.new("unsupported #{ts_name}: #{parameter_value}") if newvalue.nil?
+        raise Fasp::Error.new("unsupported #{param_name}: #{parameter_value}") if newvalue.nil?
         parameter_value=newvalue
       end
 
@@ -104,21 +110,21 @@ module Asperalm
         return parameter_value
       when :envvar # set in env var
         # define ascp parameter in env var from transfer spec
-        @result_env[env_name(ts_name,options)] = parameter_value
+        @result_env[env_name(param_name,options)] = parameter_value
       when :opt_without_arg # if present and true : just add option without value
         add_param=false
         case parameter_value
         when false# nothing to put on command line, no creation by default
         when true; add_param=true
-        else raise Fasp::Error.new("unsupported #{ts_name}: #{parameter_value}")
+        else raise Fasp::Error.new("unsupported #{param_name}: #{parameter_value}")
         end
         add_param=!add_param if options[:add_on_false]
-        add_command_line_options([switch_name(ts_name,options)]) if add_param
+        add_command_line_options([switch_name(param_name,options)]) if add_param
       when :opt_with_arg # transform into command line option with value
         #parameter_value=parameter_value.to_s if parameter_value.is_a?(Integer)
         parameter_value=[parameter_value] unless parameter_value.is_a?(Array)
         # if transfer_spec value is an array, applies option many times
-        parameter_value.each{|v|add_command_line_options([switch_name(ts_name,options),v])}
+        parameter_value.each{|v|add_command_line_options([switch_name(param_name,options),v])}
       else
         raise "Error"
       end
