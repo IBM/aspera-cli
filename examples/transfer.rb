5dc9d796370da9915c2a1826d4439316fb8c5a90
Laurent Martin <laurent@asperasoft.com>
2019-03-20 23:02:40 +0100

    use new API and added comments

--- examples/transfer.rb
+++ examples/transfer.rb
@@ -1,6 +1,5 @@
 # Example: transfer a file using one of the provided transfer agents
-# add include path relative to gem root, not necessary if gem is installed
-$LOAD_PATH.unshift(File.dirname(__FILE__)+"/../lib")
+
 require 'asperalm/fasp/local'
 require 'asperalm/fasp/listener'
 require 'asperalm/fasp/installation'
@@ -8,27 +7,20 @@ require 'asperalm/log'
 require 'asperalm/rest'
 require 'json'
 
-# example of event listener that displays events on stdout
-class MyListener < Asperalm::Fasp::Listener
-  def event_enhanced(data);STDOUT.puts(JSON.generate(data));STDOUT.flush;end
-end
+##############################################################
+# generic initialisation : configuration of FaspManager
 
-# set trace level for sample
-Asperalm::Log.instance.level=:debug
+# set trace level for sample, set to :debug to see complete list of debug information
+Asperalm::Log.instance.level=:warn
 
-# To use the "local" FASP, one way is to tell paths to mandatory files embedded in your app
-# Here we provide the list of files and main folder
-my_project_folder='/Users/laurent/Applications/Aspera Connect.app/Contents/Resources'
-my_resources={
-  :ascp               => 'ascp',
-  :ssh_bypass_key_dsa => 'asperaweb_id_dsa.openssh',
-  :ssh_bypass_key_rsa => 'aspera_tokenauth_id_rsa'
-}
-Asperalm::Fasp::Installation.instance.paths=my_resources.merge(my_resources){|k,o,n|"#{my_project_folder}/#{n}"}
+# set path to your copy of ascp binary
+Asperalm::Fasp::Installation.instance.ascp_path='/Users/laurent/Applications/Aspera Connect.app/Contents/Resources/ascp'
+# some required files are generated here (keys, certs)
+Asperalm::Fasp::Installation.instance.config_folder = '.'
 
 # another way is to detect installed products and use one of them
 #Asperalm::Fasp::Installation.instance.installed_products.each{|p|puts("found: #{p[:name]}")}
-#Asperalm::Fasp::Installation.instance.activated='Aspera Connect'
+#Asperalm::Fasp::Installation.instance.use_ascp_from_product('Aspera Connect')
 
 # get FASP Manager singleton based on above ascp location
 fasp_manager=Asperalm::Fasp::Local.instance
@@ -40,22 +32,50 @@ fasp_manager=Asperalm::Fasp::Local.instance
 #require 'asperalm/fasp/node'
 #Asperalm::Fasp::Node.instance.node_api=Asperalm::Rest.new()
 
+##############################################################
+# Optional : register an event listener
+
+# example of event listener that displays events on stdout
+class MyListener < Asperalm::Fasp::Listener
+  # this is the callback called during transfers, here we only display the received information
+  # but it could be used to get detailed error information, check "type" field is "ERROR"
+  def event_enhanced(data);STDOUT.puts(JSON.generate(data));STDOUT.flush;end
+end
+
 # register the sample listener to display events
 fasp_manager.add_listener(MyListener.new)
 
+##############################################################
 # first example: download by SSH credentials
+
+# manually build teansfer spec
 transfer_spec={
   'remote_host'     =>'demo.asperasoft.com',
   'remote_user'     =>'asperaweb',
   'remote_password' =>'demoaspera',
   'direction'       =>'receive',
   'destination_root'=>'.',
-  'paths'           =>[{'source'=>'aspera-test-dir-tiny/200KB.1'}]
+  'paths'           =>[{'source'=>'aspera-test-dir-tiny/1200KB.1'}]
 }
+# start transfer in separate thread
+# method returns as soon as transfer thread is created
+# it des not wait for completion, or even for session startup
 fasp_manager.start_transfer(transfer_spec)
-fasp_manager.wait_for_transfers_completion
 
+# optional: helper method: wait for completion of transfers
+# here we started a single transfer session (no multisession parameter)
+# get array of status, one for each session (so, a single value array)
+# each status is either :success or "error message"
+transfer_result=fasp_manager.wait_for_transfers_completion
+STDOUT.puts(JSON.generate(transfer_result))
+# get list of errors only
+errors=transfer_result.select{|i|!i.eql?(:success)}
+# the transfer was not success, as there is at least one error
+raise "Error(s) occured: #{errors.join(',')}" if !errors.empty?
+
+##############################################################
 # second example: upload with node authorization
+
 # create rest client
 node_api=Asperalm::Rest.new({
   :base_url => 'https://eudemo.asperademo.com:9092',
@@ -64,18 +84,24 @@ node_api=Asperalm::Rest.new({
   :username => 'node_aspera',
   :password => 'aspera'
   }})
-# my sources (create sample file) and destination folder
+# define sample file(s) and destination folder
 sources=['sample_file.txt']
 destination='/Upload'
+# create sample file(s)
 sources.each{|p|File.write(p,"Hello World!")}
-# request transfer authorization to node
+# request transfer authorization to node for a single transfer (This is a node api v3 call)
 send_result=node_api.create('files/upload_setup',{ :transfer_requests => [ { :transfer_request => { :paths => [ { :destination => destination } ] } } ] } )[:data]
 # we normally have only one transfer spec in list, so just get the first transfer_spec
 transfer_spec=send_result['transfer_specs'].first['transfer_spec']
-# set list of files to upload
+# add list of files to upload
 transfer_spec['paths']=sources.map{|p|{'source'=>p}}
-# use ssh bypass key
+# set authentication type to "token" (will trigger use of bypass SSH key)
 transfer_spec['authentication']='token'
+# from here : same as example 1
 fasp_manager.start_transfer(transfer_spec)
 # optional: wait for transfer completion helper function to get events
-fasp_manager.wait_for_transfers_completion
+transfer_result=fasp_manager.wait_for_transfers_completion
+errors=transfer_result.select{|i|!i.eql?(:success)}
+# the transfer was not success, as there is at least one error
+raise "Error(s) occured: #{errors.join(',')}" if !errors.empty?
+
