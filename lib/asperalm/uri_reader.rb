b6b2b71ae90b0841a96fb27c7e729e397318da07
Laurent Martin <laurent@asperasoft.com>
2019-03-25 16:06:18 +0100

    new class

---
+++ lib/asperalm/uri_reader.rb
@@ -0,0 +1,24 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+
+module Asperalm
+  module UriReader
+    # read some content from some URI, support file: , http: and https: schemes
+    def self.read(proxy_pac_uri)
+      proxy_uri=URI.parse(proxy_pac_uri)
+      if proxy_uri.scheme.eql?('http')
+        return Net::HTTP.start(proxy_uri.host, proxy_uri.port){|http|http.get(proxy_uri.path)}.body
+      elsif proxy_uri.scheme.eql?('https')
+        return Net::HTTPS.start(proxy_uri.host, proxy_uri.port){|http|http.get(proxy_uri.path)}.body
+      elsif proxy_uri.scheme.eql?('file')
+        local_file_path=proxy_uri.path
+        local_file_path=File.expand_path(local_file_path.gsub(/^\//,'')) if local_file_path.match(/^\/(~|.|..)\//)
+        return File.read(local_file_path)
+      elsif proxy_uri.scheme.eql?('')
+        return File.read(proxy_uri)
+      end
+      raise "no scheme: [#{proxy_uri.scheme}] for [#{proxy_pac_uri}]"
+    end
+  end
+end
