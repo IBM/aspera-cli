2120f68f11940fb4bda00a3fe17b4a77f5c6d889
Laurent Martin <laurent@asperasoft.com>
2019-03-25 16:07:28 +0100

    cleanup

--- lib/asperalm/fasp/node.rb
+++ lib/asperalm/fasp/node.rb
@@ -4,37 +4,45 @@ require 'singleton'
 
 module Asperalm
   module Fasp
+    # this singleton class is used by the CLI to provide a common interface to start a transfer
+    # before using it, the use must set the `node_api` member.
     class Node < Manager
       include Singleton
       private
       def initialize
         super
         @node_api=nil
-        # TODO: currently only supports one transfer
+        # TODO: currently only supports one transfer. This is bad shortcut. but ok for CLI.
         @transfer_id=nil
       end
 
+      # used internally to ensure node api is set before using.
       def node_api_
-        raise StandardError,"Before using this object, set the node_api attribute to a Asperalm::Rest object" if @node_api.nil?
+        raise StandardError,'Before using this object, set the node_api attribute to a Asperalm::Rest object' if @node_api.nil?
         return @node_api
       end
 
       public
+      # use this to read the node_api end point.
       attr_reader :node_api
 
+      # use this to set the node_api end point before using the class.
       def node_api=(new_value)
         if !@node_api.nil? and !new_value.nil?
-          Log.log.warn("overriding existing node api value")
+          Log.log.warn('overriding existing node api value')
         end
         @node_api=new_value
       end
 
+      # generic method
       def start_transfer(transfer_spec,options=nil)
         resp=node_api_.create('ops/transfers',transfer_spec)[:data]
         @transfer_id=resp['id']
         Log.log.debug("tr_id=#{@transfer_id}")
+        return @transfer_id
       end
 
+      # generic method
       def wait_for_transfers_completion
         started=false
         # lets emulate management events to display progress bar
@@ -42,18 +50,18 @@ module Asperalm
           trdata=node_api_.read("ops/transfers/#{@transfer_id}")[:data]
           case trdata['status']
           when 'completed'
-            notify_listeners("emulated",{'Type'=>'DONE'})
+            notify_listeners('emulated',{'Type'=>'DONE'})
             break
           when 'waiting','partially_completed'
             puts trdata['status']
           when 'running'
             #puts "running: sessions:#{trdata["sessions"].length}, #{trdata["sessions"].map{|i| i['bytes_transferred']}.join(',')}"
-            if !started and trdata["precalc"].is_a?(Hash) and
-            trdata["precalc"]["status"].eql?("ready")
-              notify_listeners("emulated",{'Type'=>'NOTIFICATION','PreTransferBytes'=>trdata["precalc"]["bytes_expected"]})
+            if !started and trdata['precalc'].is_a?(Hash) and
+            trdata['precalc']['status'].eql?('ready')
+              notify_listeners('emulated',{'Type'=>'NOTIFICATION','PreTransferBytes'=>trdata['precalc']['bytes_expected']})
               started=true
             else
-              notify_listeners("emulated",{'Type'=>'STATS','Bytescont'=>trdata["bytes_transferred"]})
+              notify_listeners('emulated',{'Type'=>'STATS','Bytescont'=>trdata['bytes_transferred']})
             end
           else
             Log.log.warn("trdata -> #{trdata}")
