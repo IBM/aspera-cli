a7c2352df8ba7bbc25fe24ae52426175bcecd350
Laurent Martin <laurent@asperasoft.com>
2019-03-27 16:13:36 +0100

    compact

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
