cb348b7ad28c16cee55dcf3defe897d1a13217f2
laurent <laurent@localhost>
2019-10-19 00:08:44 +0200

    clarify error message

--- lib/asperalm/cli/main.rb
+++ lib/asperalm/cli/main.rb
@@ -255,7 +255,7 @@ module Asperalm
       end
 
       def result_all_fields(results,table_rows_hash_val)
-        raise "empty" if table_rows_hash_val.empty?
+        raise "cannot get field names for empty result" if table_rows_hash_val.empty?
         if table_rows_hash_val.is_a?(Array)
           # get the list of all column names used in all lines, not just frst one, as all lines may have different columns
           final_table_columns=table_rows_hash_val.inject({}){|m,v|v.keys.each{|c|m[c]=true};m}.keys
