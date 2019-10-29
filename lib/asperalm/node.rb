0c6d003f14a5f7613508f8334cb8211f0684ef23
laurent <laurent@laurents-mbp.biot.fr.ibm.com>
2019-10-29 12:32:33 +0100

    initial

---
+++ lib/asperalm/node.rb
@@ -0,0 +1,9 @@
+module Asperalm
+  # Provides additional functions using node API.
+  class Node < Rest
+    def initialize(rest_params)
+      super(rest_params)
+      # specifics here
+    end
+  end
+end
