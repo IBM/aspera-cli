0aeab27547c090955c1d5d7bd3d24f9d63095e19
laurent <laurent@localhost>
2019-12-04 23:15:18 +0100

    add suffix for better uniqueness

--- lib/asperalm/fasp/parameters.rb
+++ lib/asperalm/fasp/parameters.rb
@@ -16,7 +16,7 @@ module Asperalm
       # temp folder for file lists, must contain only file lists
       # because of garbage collection takes any file there
       # this could be refined, as , for instance, on macos, temp folder is already user specific
-      @@file_list_folder=File.join(Etc.systmpdir,Etc.getlogin)
+      @@file_list_folder=File.join(Etc.systmpdir,Etc.getlogin)+'_asession_filelists'
       SEC_IN_DAY=86400
       # assume no transfer last longer than this
       # (garbage collect file list which were not deleted after transfer)
