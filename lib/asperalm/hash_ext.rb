3840b957833c70f381b332bc9c9d64f9b8a51f1e
laurent <laurent@localhost>
2020-05-08 02:08:00 +0200

    add symbolize_keys

--- lib/asperalm/hash_ext.rb
+++ lib/asperalm/hash_ext.rb
@@ -18,3 +18,11 @@ class ::Hash
     self.merge!(second){|key,v1,v2|Hash===v1&&Hash===v2 ? v1.deep_merge!(v2) : v2}
   end
 end
+
+unless Hash.method_defined?(:symbolize_keys)
+  class Hash
+    def symbolize_keys
+      return self.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+    end
+  end
+end
