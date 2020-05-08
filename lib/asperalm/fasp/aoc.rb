01a85517b9490a91f9da77e47c71db037dd7254f
laurent <laurent@localhost>
2020-05-08 05:03:39 +0200

    cleanup

--- lib/asperalm/fasp/aoc.rb
+++ lib/asperalm/fasp/aoc.rb
@@ -5,8 +5,6 @@ require 'asperalm/on_cloud.rb'
 module Asperalm
   module Fasp
     class Aoc < Manager
-      SYMBOL_OPTS=["auth"]
-
       def initialize(on_cloud_options)
         super()
         @api_oncloud=OnCloud.new(on_cloud_options)
