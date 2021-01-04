ad557e5edbf6f49a572d60a8bd193fcc44854331
Laurent Martin <laurent.martin.l@gmail.com>
2021-01-04 09:36:33 +0100

    allow nil subpath

--- lib/aspera/rest.rb
+++ lib/aspera/rest.rb
@@ -171,7 +171,7 @@ module Aspera
       end
       # TODO: shall we percent encode subpath (spaces) test with access key delete with space in id
       # URI.escape()
-      uri=self.class.build_uri("#{@params[:base_url]}/#{call_data[:subpath]}",call_data[:url_params])
+      uri=self.class.build_uri("#{@params[:base_url]}#{call_data[:subpath].nil? ? '' : '/'}#{call_data[:subpath]}",call_data[:url_params])
       Log.log.debug("URI=#{uri}")
       begin
         # instanciate request object based on string name
