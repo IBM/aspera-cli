53c81801fa389730f70fe0d99f0f9f296f980188
Laurent Martin <laurent.martin.l@gmail.com>
2021-02-06 09:38:53 +0100

    Revert "add upload"
    This reverts commit a0b7cb2f1ba3b213ec6f9ec89a998559e08aaf27.

--- lib/aspera/fasp/http_gw.rb
+++ lib/aspera/fasp/http_gw.rb
@@ -2,117 +2,10 @@
 require 'aspera/fasp/manager'
 require 'aspera/log'
 require 'aspera/rest'
-require 'socket'
-require 'websocket' # https://rdoc.info/github/imanel/websocket-ruby/frames
-require 'openssl'
-require 'base64'
-require 'json'
-require 'uri'
 
 # ref: https://api.ibm.com/explorer/catalog/aspera/product/ibm-aspera/api/http-gateway-api/doc/guides-toc
 module Aspera
   module Fasp
-    class SimpleWs
-      def initialize(url)
-        url=url.gsub(/^http/,'ws')
-        handshake=WebSocket::Handshake::Client.new(url: url, headers: { 'User-Agent' => 'ascli/4.0' })
-        uri=URI.parse(url)
-        @socket  = TCPSocket.new(uri.host, uri.port)
-        if uri.scheme.eql?('wss')
-          @socket = OpenSSL::SSL::SSLSocket.new(@socket)
-          @socket.sync_close = true
-          @socket.connect
-          @socket.write(handshake.to_s)
-        end
-        handshake << @socket.readpartial(1024)
-        raise "not finished" unless handshake.finished?
-        raise "not valid" unless handshake.valid?
-        @version=handshake.version
-        @fr_from_server ||= WebSocket::Frame::Incoming::Server.new(version: @version)
-        @process_from_server=nil
-      end
-
-      def process_from_server(&block)
-        @process_from_server=block
-      end
-
-      def send_message(textdata)
-        fr_to_server = WebSocket::Frame::Outgoing::Client.new(version: @version, data: textdata, type: :text)
-        @socket.write(fr_to_server.to_s)
-        if @socket.eof?
-          raise "premature"
-          return
-        end
-        readnet=@socket.readpartial(65536)
-        @fr_from_server << readnet
-        while f=@fr_from_server.next
-          #puts "<<#{f.type}<#{f.to_s}"
-          if f.type.eql?(:text)
-            @process_from_server.call(f.to_s)
-          else
-            puts("ignore: error? #{f.error?}")
-            puts("ignore: f #{f}")
-          end
-        end
-        raise "@fr_from_server error:#{@fr_from_server.error}" if @fr_from_server.error?
-      end
-    end
-
-    class FileSender
-      OK_MESSAGE="end upload"
-      def initialize(url)
-        @chunksize=128000 # todo configurable ?
-        @ws=SimpleWs.new(url)
-        # process_from_server message from server
-        @ws.process_from_server do |message|
-          if !message.eql?(OK_MESSAGE)
-            message.chomp!
-            raise "expecting quotes in [#{message}]" unless message[0].eql?('"') and message[-1].eql?('"')
-            err=JSON.parse(Base64.strict_decode64(message.chomp[1..-2]))
-            raise err['message']
-          end
-        end
-      end
-
-      def send_msg(obj)
-        @ws.send_message(JSON.generate(obj))
-      end
-
-      def upload(transfer_spec)
-        #send_msg("xxx")
-        send_msg({transfer_spec: transfer_spec})
-        filenum=0
-        transfer_spec['paths'].each do |item|
-          source_path=item['source']
-          destination_path=source_path
-          file_type=''
-          STDOUT.write("#{source_path}")
-          total=File.size(source_path)
-          numslices=1+(total-1)/@chunksize
-          slicenum=0
-          File.open(source_path) do |file|
-            while !file.eof? do
-              STDOUT.write(".")
-              data=file.read(@chunksize)
-              slice_data={
-                name: destination_path,
-                type: file_type,
-                size: total,
-                data: Base64.strict_encode64(data),
-                slice: slicenum,
-                total_slices: numslices,
-                fileIndex: filenum
-              }
-              send_msg({slice_upload: slice_data})
-              slicenum+=1
-            end
-            STDOUT.write("\n")
-          end
-          filenum+=1
-        end
-      end
-    end
-
     # executes a local "ascp", connects mgt port, equivalent of "Fasp Manager"
     class HttpGW < Manager
       # start FASP transfer based on transfer spec (hash table)
@@ -122,15 +15,13 @@ module Aspera
         raise "GW URL must be set" unless !@gw_api.nil?
         raise "option: must be hash (or nil)" unless options.is_a?(Hash)
         raise "paths: must be Array" unless transfer_spec['paths'].is_a?(Array)
-        raise "on token based transfer is supported in GW" unless transfer_spec['token'].is_a?(String)
-        transfer_spec['authentication']||='token'
         case transfer_spec['direction']
         when 'send'
           # this is a websocket
-          #raise "error, not implemented"
-          FileSender.new("#{@gw_api.params[:base_url]}/upload").upload(transfer_spec)
+          raise "error, not implemented"
         when 'receive'
           transfer_spec['zip_required']||=false
+          transfer_spec['authentication']||='token'
           transfer_spec['source_root']||='/'
           # is normally provided by application, like package name
           if !transfer_spec.has_key?('download_name')
