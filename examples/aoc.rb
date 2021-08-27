92584bb747049433f50e07cee9e6df122b649a83
Laurent Martin <laurent.martin.l@gmail.com>
2021-08-27 11:00:48 +0200

    test the examples

--- examples/aoc.rb
+++ examples/aoc.rb
@@ -4,11 +4,22 @@ require 'aspera/log'
 
 Aspera::Log.instance.level=:debug
 
+if ! ARGV.length.eql?(3)
+  Aspera::Log.log.error("wrong number of args: #{ARGV.length}")
+  Aspera::Log.log.error("Usage: #{$0} <aoc URL> <aoc username> <aoc private key content>")
+  Aspera::Log.log.error("Example: #{$0} https://myorg.ibmaspera.com john@example.com $(cat /home/john/my_key.pem)")
+  Process.exit(1)
+end
+
+aoc_url=ARGV[0]
+aoc_user=ARGV[1]
+aoc_key_value=ARGV[2]
+
 aocapi=Aspera::AoC.new(
-url: 'https://myorg.ibmaspera.com',
+url: aoc_url,
 auth: :jwt,
-private_key: File.read('path/to_your_private_key.pem'),
-username: 'my.email@example.com',
+private_key: aoc_key_value,
+username: aoc_user,
 scope: 'user:all',
 subpath: 'api/v1')
 
