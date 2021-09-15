8ce153364ec869ed04059d85fbd0341980116dd8
Laurent Martin <laurent.martin.l@gmail.com>
2021-09-15 15:44:35 +0200

    example use tmp dir

--- examples/faspex4.rb
+++ examples/faspex4.rb
@@ -2,15 +2,15 @@
 require 'aspera/rest'
 require 'aspera/log'
 require 'aspera/fasp/local'
-require 'aspera/cli/listener/line_dump'
-require 'aspera/cli/extended_value'
 
-# set high log level for the example
+tmpdir=ENV['tmp']||Dir.tmpdir || '.'
+
+# Set high log level for the example
 Aspera::Log.instance.level=:debug
 
-# set folder where SDK is installed
+# Set folder where SDK is installed
 # (if ascp is not there, the lib will try to find in usual locations)
-Aspera::Fasp::Installation.instance.folder='.'
+Aspera::Fasp::Installation.instance.folder = tmpdir
 
 if ! ARGV.length.eql?(3)
   Aspera::Log.log.error("wrong number of args: #{ARGV.length}")
@@ -35,34 +35,36 @@ api_v3=Aspera::Rest.new({
   :password => faspex_pass
   }})
 
+# very simple api call
 api_v3.read('me')
 
 # 2: send a package
 #---------------
 
 # create a sample file to send
-file_to_send='./myfile.bin'
+file_to_send=File.join(tmpdir,'myfile.bin')
 File.open(file_to_send, "w") {|f| f.write("sample data") }
 # package creation parameters
 package_create_params={'delivery'=>{'title'=>'test package','recipients'=>['aspera.user1@gmail.com'],'sources'=>[{'paths'=>[file_to_send]}]}}
 pkg_created=api_v3.create('send',package_create_params)[:data]
-# get transfer specification
+# get transfer specification (normally: only one)
 transfer_spec=pkg_created['xfer_sessions'].first
 # set paths of files to send
 transfer_spec['paths']=['source'=>file_to_send]
 # get the local agent (i.e. ascp)
 client=Aspera::Fasp::Local.new
-# disable ascp output on stdout to not mix with JSON events
+# disable ascp output on stdout (optional)
 client.quiet=true
 # start transfer (asynchronous)
 job_id=client.start_transfer(transfer_spec)
+# wait for all transfer completion (for the example)
 result=client.wait_for_transfers_completion
 #  notify of any transfer error
 result.select{|i|!i.eql?(:success)}.each do |e|
   Aspera::Log.log.error("A transfer error occured: #{e.message}")
 end
 
-# 1: demo API v4
+# 3: demo API v4
 #---------------
 api_v4=Aspera::Rest.new({
   :base_url  => faspex_url+'/api',
