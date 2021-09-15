6704a76ba2435778eda44d5a227364102f1a1d43
Laurent Martin <laurent.martin.l@gmail.com>
2021-09-15 21:21:44 +0200

    clarify

--- examples/faspex4.rb
+++ examples/faspex4.rb
@@ -1,32 +1,37 @@
 #!/usr/bin/env ruby
+# find Faspex API here: https://developer.ibm.com/apis/catalog/?search=faspex
+# this example makes use of class Aspera::Rest for REST calls, alternatively class RestClient of gem rest-client could be used
+# this example makes use of class Aspera::Fasp::Local for transfers, alternatively the official "Transfer SDK" could be used
+# Aspera SDK can be downloaded with: `ascli conf ascp install` , it installs in $HOME/.aspera/ascli/sdk
 require 'aspera/rest'
 require 'aspera/log'
 require 'aspera/fasp/local'
 
 tmpdir=ENV['tmp']||Dir.tmpdir || '.'
 
-# Set high log level for the example
+# Set high log level for the example, decrease to :warn usually
 Aspera::Log.instance.level=:debug
 
-# Set folder where SDK is installed
+# Set folder where SDK is installed (mandatory)
 # (if ascp is not there, the lib will try to find in usual locations)
+# (if data files are not there, they will be created)
 Aspera::Fasp::Installation.instance.folder = tmpdir
 
 if ! ARGV.length.eql?(3)
-  Aspera::Log.log.error("wrong number of args: #{ARGV.length}")
+  Aspera::Log.log.error("Wrong number of args: #{ARGV.length}")
   Aspera::Log.log.error("Usage: #{$0} <faspex URL> <faspex username> <faspex password>")
   Aspera::Log.log.error("Example: #{$0} https://faspex.com/aspera/faspex john p@sSw0rd")
   Process.exit(1)
 end
 
-faspex_url=ARGV[0]
+faspex_url=ARGV[0] # typically: https://faspex.example.com/aspera/faspex
 faspex_user=ARGV[1]
 faspex_pass=ARGV[2]
 
-# comment out this if certificate is valid, keep to ignore certificate
+# comment out this if certificate is valid, keep line to ignore certificate
 Aspera::Rest.insecure=true
 
-# 1: demo API v3
+# 1: Faspex 4 API v3
 #---------------
 
 # create REST API object
@@ -53,21 +58,21 @@ pkg_created=api_v3.create('send',package_create_params)[:data]
 # get transfer specification (normally: only one)
 transfer_spec=pkg_created['xfer_sessions'].first
 # set paths of files to send
-transfer_spec['paths']=['source'=>file_to_send]
+transfer_spec['paths']=[{'source'=>file_to_send}]
 # get the local agent (i.e. ascp)
-client=Aspera::Fasp::Local.new
+transfer_client=Aspera::Fasp::Local.new
 # disable ascp output on stdout (optional)
-client.quiet=true
+transfer_client.quiet=true
 # start transfer (asynchronous)
-job_id=client.start_transfer(transfer_spec)
+job_id=transfer_client.start_transfer(transfer_spec)
 # wait for all transfer completion (for the example)
-result=client.wait_for_transfers_completion
+result=transfer_client.wait_for_transfers_completion
 #  notify of any transfer error
 result.select{|i|!i.eql?(:success)}.each do |e|
   Aspera::Log.log.error("A transfer error occured: #{e.message}")
 end
 
-# 3: demo API v4
+# 3: Faspex 4 API v4
 #---------------
 api_v4=Aspera::Rest.new({
   :base_url  => faspex_url+'/api',
@@ -80,4 +85,5 @@ api_v4=Aspera::Rest.new({
   :scope     => 'admin'
   }})
 
+# Use it. Note that Faspex 4 API v4 is totally different from Faspex 4 v3 APIs, see ref on line 2
 Aspera::Log.dump('users',api_v4.read('users')[:data])
