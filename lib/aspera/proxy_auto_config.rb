f0aa8339ff5c5a29d685ea40a81a3f01ae60ef90
Laurent Martin <laurent.martin.l@gmail.com>
2022-03-04 11:23:14 +0100

    no erb

--- lib/aspera/proxy_auto_config.rb
+++ lib/aspera/proxy_auto_config.rb
@@ -1,27 +1,36 @@
 require 'uri'
 require 'resolv'
-require 'erb'
 
 module Aspera
   # Evaluate a proxy autoconfig script
   class ProxyAutoConfig
     # template file is read once, it contains functions that can be used in a proxy autoconf script
     # it is similar to mozilla ascii_pac_utils.inc
-    PAC_UTILS_TEMPLATE=__FILE__.gsub(/\.rb$/,'.erb.js').freeze
+    PAC_FUNCTIONS_FILE=__FILE__.gsub(/\.rb$/,'.js').freeze
     PAC_MAIN_FUNCTION='FindProxyForURL'.freeze
-    private_constant :PAC_UTILS_TEMPLATE,:PAC_MAIN_FUNCTION
+    private_constant :PAC_FUNCTIONS_FILE,:PAC_MAIN_FUNCTION
 
     private
     # variables starting with "context_" are replaced in the ERB template file
     # I did not find an easy way for the javascript to callback ruby
     # and anyway, it only needs to get DNS translation
-    def build_binding(context_host)
+    def pac_dns_functions(context_host)
       context_self='127.0.0.1'
       context_ip=nil
       Resolv::DNS.open{|dns|dns.each_address(context_host){|r_addr|context_ip=r_addr.to_s if r_addr.is_a?(Resolv::IPv4)}}
       raise "DNS name not found: #{context_host}" if context_ip.nil?
-      # Kernel.binding contains current local variables
-      return Kernel.binding
+      # NOTE: Javascript code here with string inclusions
+      javascript=<<END_OF_JAVASCRIPT
+      function dnsResolve(host) {
+        if (host == '#{context_host}' || host == '#{context_ip}')
+          return '#{context_ip}';
+        throw 'only DNS for initial host, not ' + host;
+      }
+      function myIpAddress() {
+        return '#{context_self}';
+      }
+END_OF_JAVASCRIPT
+      return javascript
     end
 
     public
@@ -32,7 +41,6 @@ module Aspera
       @proxy_auto_config=proxy_auto_config
       # avoid multiple execution, this does not support load balancing
       @cache={}
-      @lib_template=nil
     end
 
     # execute proxy auto config script for the given URL : https://en.wikipedia.org/wiki/Proxy_auto-config
@@ -41,16 +49,16 @@ module Aspera
       uri=URI.parse(service_url)
       simple_url="#{uri.scheme}://#{uri.host}"
       if !@cache.has_key?(simple_url)
+        Log.log.debug("PAC: starting javascript for #{service_url}")
         # require at runtime, in case there is no js engine
         require 'execjs'
         # read template lib
-        @lib_template=File.read(PAC_UTILS_TEMPLATE).freeze if @lib_template.nil?
-        # generate javascript library with dns resolution
-        pac_utils_with_resolve=ERB.new(@lib_template).result(build_binding(uri.host))
-        # to be executed is utils + user function
-        js_to_execute=pac_utils_with_resolve+@proxy_auto_config
+        @pac_functions=File.read(PAC_FUNCTIONS_FILE).freeze if @pac_functions.nil?
+        # to be executed is dns + utils + user function
+        js_to_execute="#{pac_dns_functions(uri.host)}#{@pac_functions}#{@proxy_auto_config}"
         executable_js = ExecJS.compile(js_to_execute)
         @cache[simple_url]=executable_js.call(PAC_MAIN_FUNCTION, simple_url, uri.host)
+        Log.log.debug("PAC: result: #{@cache[simple_url]}")
       end
       return @cache[simple_url]
     end
