a5d681b30f6e59ab3e72d9956127b1d55d32623b
Laurent Martin <laurent.martin.l@gmail.com>
2022-04-01 18:44:13 +0200

    Hash.dig is part of ruby since 2.3

--- lib/aspera/hash_ext.rb
+++ lib/aspera/hash_ext.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# for older rubies
-unless Hash.method_defined?(:dig)
-  class Hash
-    def dig(*path)
-      path.inject(self) do |location, key|
-        location.respond_to?(:keys) ? location[key] : nil
-      end
-    end
-  end
-end
-
 class ::Hash
   def deep_merge(second)
     merge(second){|_key,v1,v2|Hash === v1 && Hash === v2 ? v1.deep_merge(v2) : v2}
