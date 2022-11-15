598dd5bf038079a846e1482ebfcec0fa1031522a
Laurent Martin <laurent.martin.l@gmail.com>
2022-11-15 18:45:12 +0100

    bump future minimum ruby to 2.7

--- lib/aspera/cli/info.rb
+++ lib/aspera/cli/info.rb
@@ -10,9 +10,9 @@ module Aspera
     GEM_URL  = "https://rubygems.org/gems/#{GEM_NAME}"
     SRC_URL  = 'https://github.com/IBM/aspera-cli'
     # set this to warn in advance when minimum required ruby version will increase
-    # for example currently minimum version is 2.4 in gemspec, but future minimum will be 2.5
+    # for example currently minimum version is 2.4 in gemspec, but future minimum will be different
     # set to current minimum if there is no deprecation
     # the actual current minimum required version is in gemspec at required_ruby_version
-    RUBY_FUTURE_MINIMUM_VERSION = '2.5'
+    RUBY_FUTURE_MINIMUM_VERSION = '2.7'
   end
 end
