40e56b2c91640fe29676eddadc4dbe346effca0b
Laurent Martin <laurent.martin.l@gmail.com>
2023-01-25 09:03:17 +0100

    test coverage (sync)

--- lib/aspera/cli/plugins/sync.rb
+++ lib/aspera/cli/plugins/sync.rb
@@ -9,12 +9,14 @@ require 'open3'
 module Aspera
   module Cli
     module Plugins
-      # list and download connect client versions, select FASP implementation
+      # Execute Aspera Sync
       class Sync < Plugin
+        ASYNC_EXECUTABLE = 'async'
+        ASYNC_ADMIN_EXECUTABLE = 'asyncadmin'
         def initialize(env)
           super(env)
-          options.add_opt_simple(:parameters, 'extended value for session set definition')
-          options.add_opt_simple(:session_name, 'name of session to use for admin commands, by default first one')
+          options.add_opt_simple(:parameters, 'Extended value for session set definition')
+          options.add_opt_simple(:session_name, 'Name of session to use for admin commands. default: first in parameters')
           options.parse_options!
         end
 
@@ -25,9 +27,8 @@ module Aspera
           case command
           when :start
             env_args = Aspera::Sync.new(options.get_option(:parameters, is_type: :mandatory)).compute_args
-            async_bin = 'async'
-            Log.log.debug{"execute: #{env_args[:env].map{|k, v| "#{k}=\"#{v}\""}.join(' ')} \"#{async_bin}\" \"#{env_args[:args].join('" "')}\""}
-            res = system(env_args[:env], [async_bin, async_bin], *env_args[:args])
+            Log.log.debug{"execute: #{env_args[:env].map{|k, v| "#{k}=\"#{v}\""}.join(' ')} \"#{ASYNC_EXECUTABLE}\" \"#{env_args[:args].join('" "')}\""}
+            res = system(env_args[:env], [ASYNC_EXECUTABLE, ASYNC_EXECUTABLE], *env_args[:args])
             Log.log.debug{"result=#{res}"}
             case res
             when true then return Main.result_success
@@ -38,19 +39,24 @@ module Aspera
           when :admin
             p = options.get_option(:parameters, is_type: :mandatory)
             n = options.get_option(:session_name)
-            cmdline = ['asyncadmin', '--quiet']
+            cmdline = [ASYNC_ADMIN_EXECUTABLE, '--quiet']
             session = n.nil? ? p['sessions'].first : p['sessions'].find{|s|s['name'].eql?(n)}
+            raise 'Session not found' if session.nil?
+            raise 'Missing session name' if session['name'].nil?
             cmdline.push('--name=' + session['name'])
             if session.key?('local_db_dir')
               cmdline.push('--local-db-dir=' + session['local_db_dir'])
-            else
+            elsif session.key?('local_dir')
               cmdline.push('--local-dir=' + session['local_dir'])
+            else
+              raise 'Missing either local_db_dir or local_dir'
             end
             command2 = options.get_next_command([:status])
             case command2
             when :status
               stdout, stderr, status = Open3.capture3(*cmdline)
               Log.log.debug{"status=#{status}, stderr=#{stderr}"}
+              raise "Sync failed: #{status.exitstatus} : #{stderr}" unless status.success?
               items = stdout.split("\n").each_with_object({}){|l, m|i = l.split(/:  */); m[i.first.lstrip] = i.last.lstrip} # rubocop:disable Style/Semicolon
               return {type: :single_object, data: items}
             else raise 'error'
