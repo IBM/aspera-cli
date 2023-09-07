e234f261e0ba9110665e3972137a1bae42988586
Laurent Martin <laurent.martin.l@gmail.com>
2023-09-08 01:03:32 +0200

    better warning message

--- lib/aspera/cli/main.rb
+++ lib/aspera/cli/main.rb
@@ -87,7 +87,7 @@ module Aspera
         if @option_insecure
           url = http.inspect.gsub(/^[^ ]* /, 'https://').gsub(/ [^ ]*$/, '')
           if !@ssl_warned_urls.include?(url)
-            @formatter.display_message(:error, "#{WARNING_FLASH} ignoring certificate for: #{url}. Do not use unsafe certificates in production.")
+            @formatter.display_message(:error, "#{WARNING_FLASH} ignoring certificate for: #{url}. Do not deactivate certificate verification in production.")
             @ssl_warned_urls.push(url)
           end
           http.verify_mode = SELF_SIGNED_CERT
