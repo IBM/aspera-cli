38294f1aca46ecbab9be3b5050e42895dbb6e62b
Laurent Martin <laurent.martin.l@gmail.com>
2023-10-02 18:22:44 +0200

    fix detection of upload_setup error

--- lib/aspera/rest_errors_aspera.rb
+++ lib/aspera/rest_errors_aspera.rb
@@ -20,40 +20,32 @@ module Aspera
         RestErrorAnalyzer.instance.add_simple_handler('Type 5', 'error_description')
         RestErrorAnalyzer.instance.add_simple_handler('Type 6', 'message')
         RestErrorAnalyzer.instance.add_handler('Type 7: errors[]') do |name, call_context|
-          if call_context[:data].is_a?(Hash) && call_context[:data]['errors'].is_a?(Hash)
-            call_context[:data]['errors'].each do |k, v|
-              RestErrorAnalyzer.add_error(call_context, name, "#{k}: #{v}")
-            end
+          next unless call_context[:data].is_a?(Hash) && call_context[:data]['errors'].is_a?(Hash)
+          call_context[:data]['errors'].each do |k, v|
+            RestErrorAnalyzer.add_error(call_context, name, "#{k}: #{v}")
           end
         end
         # call to upload_setup and download_setup of node api
         RestErrorAnalyzer.instance.add_handler('T8:node: *_setup') do |type, call_context|
-          if call_context[:data].is_a?(Hash)
-            d_t_s = call_context[:data]['transfer_specs']
-            if d_t_s.is_a?(Array)
-              d_t_s.each do |res|
-                # r_err=res['transfer_spec']['error']
-                r_err = res['error']
-                if r_err.is_a?(Hash)
-                  RestErrorAnalyzer.add_error(call_context, type, "#{r_err['code']}: #{r_err['reason']}: #{r_err['user_message']}")
-                end
-              end
-            end
+          next unless call_context[:data].is_a?(Hash)
+          d_t_s = call_context[:data]['transfer_specs']
+          next unless d_t_s.is_a?(Array)
+          d_t_s.each do |res|
+            r_err = res.dig(*%w[transfer_spec error])
+            next unless r_err.is_a?(Hash)
+            RestErrorAnalyzer.add_error(call_context, type, "#{r_err['code']}: #{r_err['reason']}: #{r_err['user_message']}")
           end
         end
         RestErrorAnalyzer.instance.add_simple_handler('T9:IBM cloud IAM', 'errorMessage')
         RestErrorAnalyzer.instance.add_simple_handler('T10:faspex v4', 'user_message')
         RestErrorAnalyzer.instance.add_handler('bss graphql') do |type, call_context|
-          if call_context[:data].is_a?(Hash)
-            d_t_s = call_context[:data]['errors']
-            if d_t_s.is_a?(Array)
-              d_t_s.each do |res|
-                r_err = res['message']
-                if r_err.is_a?(String)
-                  RestErrorAnalyzer.add_error(call_context, type, r_err)
-                end
-              end
-            end
+          next unless call_context[:data].is_a?(Hash)
+          d_t_s = call_context[:data]['errors']
+          next unless d_t_s.is_a?(Array)
+          d_t_s.each do |res|
+            r_err = res['message']
+            next unless r_err.is_a?(String)
+            RestErrorAnalyzer.add_error(call_context, type, r_err)
           end
         end
       end # register_handlers
