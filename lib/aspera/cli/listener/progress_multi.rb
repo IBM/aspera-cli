d9f2ffe3e5372d1290801aa3b45b5dead068f7ff
Laurent Martin <laurent.martin.l@gmail.com>
2023-11-03 15:45:14 +0100

    avoid multiple full line , and display speed ok

--- lib/aspera/cli/listener/progress_multi.rb
+++ lib/aspera/cli/listener/progress_multi.rb
@@ -12,14 +12,70 @@ module Aspera
       class ProgressMulti < Aspera::Fasp::Listener
         def initialize
           super
-          @progress_bar = nil
-          @sessions = {}
+          reset
         end
 
         def reset
           @progress_bar = nil
+          # key is session id
           @sessions = {}
         end
+
+        def event_enhanced(data)
+          return if @progress_bar.eql?(:transfer_complete)
+          if @progress_bar.nil?
+            @progress_bar = ProgressBar.create(
+              format:      '%t %a %B %p%% %r Mbps %E',
+              rate_scale:  lambda{|rate|rate / Environment::BYTES_PER_MEBIBIT},
+              title:       '',
+              total:       nil)
+          end
+          # check that we have a session id
+          session_id = data[Fasp::AgentBase::LISTENER_SESSION_ID_S]
+          if session_id.nil?
+            Log.log.error{"Internal error: no #{Fasp::AgentBase::LISTENER_SESSION_ID_S} in event: #{data}"}
+            return
+          end
+          new_title = @sessions.length < 2 ? '' : "multi=#{@sessions.length}"
+          @progress_bar.title = new_title unless @progress_bar.title.eql?(new_title)
+          # get or init the session
+          session_info = @sessions[session_id] ||= {
+            job_size:   0, # total size of transfer (pre-calc)
+            cumulative: 0,
+            current:    0
+          }
+          case data['type']
+          when 'INIT' # connection to ascp (get id)
+          when 'SESSION' # session information
+          when 'NOTIFICATION' # sent from remote
+            if data.key?('pre_transfer_bytes')
+              session_info[:job_size] = data['pre_transfer_bytes']
+              update_total
+            end
+          when 'STATS' # during transfer
+            if @progress_bar.total.nil?
+              @progress_bar.increment
+            else
+              session_info[:current] = data.key?('bytescont') ? session_info[:cumulative] + data['bytescont'].to_i : data['transfer_bytes'].to_i
+              update_progress
+            end
+          when 'STOP'
+            # stop event when one file is completed
+            session_info[:cumulative] = session_info[:cumulative] + data['size'].to_i
+          when 'DONE' # end of session
+            # @sessions.delete(session_id)
+            update_progress
+            update_total
+          else
+            Log.log.debug{"ignore: #{data['type']}"}
+          end
+          if @progress_bar.total.eql?(@progress_bar.progress)
+            @progress_bar.finish
+            @progress_bar = :transfer_complete
+          end
+        end
+
+        private
 
         def update_total
           @progress_bar.total = @sessions.values.inject(0){|m, s|m + s[:job_size].to_i}
@@ -31,52 +87,6 @@ module Aspera
           @progress_bar.progress = @sessions.values.inject(0){|m, s|m + s[:current].to_i}
         rescue StandardError
           nil
-        end
-
-        def event_enhanced(data)
-          if @progress_bar.nil?
-            @progress_bar = ProgressBar.create(
-              format:      '%t %a %B %p%% %r Mbps %e',
-              rate_scale:  lambda{|rate|rate / Environment::BYTES_PER_MEBIBIT},
-              title:       '',
-              total:       nil)
-          end
-          if !data.key?(Fasp::AgentBase::LISTENER_SESSION_ID_S)
-            Log.log.error{"Internal error: no #{Fasp::AgentBase::LISTENER_SESSION_ID_S} in event: #{data}"}
-            return
-          end
-          new_title = @sessions.length < 2 ? '' : "multi=#{@sessions.length}"
-          @progress_bar.title = new_title unless @progress_bar.title.eql?(new_title)
-          session = @sessions[data[Fasp::AgentBase::LISTENER_SESSION_ID_S]] ||= {
-            cumulative: 0,
-            job_size:   0,
-            current:    0
-          }
-          case data['type']
-          when 'INIT' # connection to ascp (get id)
-          when 'SESSION' # session information
-          when 'NOTIFICATION' # sent from remote
-            if data.key?('pre_transfer_bytes')
-              session[:job_size] = data['pre_transfer_bytes']
-              update_total
-            end
-          when 'STATS' # during transfer
-            if @progress_bar.total.nil?
-              @progress_bar.increment
-            else
-              session[:current] = data.key?('bytescont') ? session[:cumulative] + data['bytescont'].to_i : data['transfer_bytes'].to_i
-              update_progress
-            end
-          when 'STOP'
-            # stop event when one file is completed
-            session[:cumulative] = session[:cumulative] + data['size'].to_i
-          when 'DONE' # end of session
-            @sessions.delete(data[Fasp::AgentBase::LISTENER_SESSION_ID_S])
-            update_progress
-            update_total
-          else
-            Log.log.debug{"ignore: #{data['type']}"}
-          end
         end
       end
     end
