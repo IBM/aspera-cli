8a6eaad402207e4abecd5fa4e06bfd33f02b5108
Laurent Martin <laurent.martin.l@gmail.com>
2024-02-15 00:02:04 +0100

    remove warnings in actions

--- spec/aspera-cli_spec.rb
+++ spec/aspera-cli_spec.rb
@@ -34,7 +34,7 @@ ssh_url = URI.parse(params[:url])
 
 # main folder relative to docroot and server executor
 PATH_FOLDER_MAIN = '/'
-demo_executor = Aspera::Ssh.new(ssh_url.host, params[:user], {password: params[:pass], port: ssh_url.port})
+demo_executor = Aspera::Ssh.new(ssh_url.host, params[:user], {password: params[:pass], port: ssh_url.port, use_agent: false})
 
 # to use a local executor, set PATH_FOLDER_MAIN to the pseudo docroot (local) folder
 # PATH_FOLDER_MAIN='/pseudo/docroot'
