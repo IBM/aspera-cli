715339ae9c1d9abc1d9f0d6eed64f35242d8c150
Laurent Martin <laurent.martin.l@gmail.com>
2025-05-05 22:31:19 +0200

    spec use single env var as json

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+# Parameters are provided with env var RSPEC_CONFIG set to a json with url, username, password
+
 require 'bundler/setup'
+require 'json'
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
   config.expect_with(:rspec) do |c|
     c.syntax = :expect
+  end
+  raise 'Missing env var: RSPEC_CONFIG' unless ENV.key?('RSPEC_CONFIG')
+  params = JSON.parse(ENV['RSPEC_CONFIG'])
+  %i[url username password].each do |p|
+    param = params[p.to_s]
+    raise "Missing parameter: #{p}" if param.nil?
+    raise "Wrong value for parameter: #{p}" unless param.is_a?(String) && !param.empty?
+    config.add_setting(p, default: param)
   end
 end
