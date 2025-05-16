a16e4173cc053a8ed4a7dcda6062adbb8897c5dd
Laurent Martin <laurent.martin.l@gmail.com>
2025-05-16 14:35:29 +0200

    update

--- docs/install.bat
+++ docs/install.bat
@@ -5,13 +5,13 @@ ECHO Aspera CLI Installer script for Windows
 REM This is the installation folder
 SET TARGET_FOLDER=%USERPROFILE%\aspera-cli
 ECHO Installing Aspera CLI in %TARGET_FOLDER%
-rubyinstaller-devkit-3.2.2-1-x64.exe /silent /currentuser /noicons /dir=%TARGET_FOLDER%
+rubyinstaller-devkit-3.3.8-1-x64.exe /silent /currentuser /noicons /dir=%TARGET_FOLDER%
 ECHO Installing CLI gems
 REM On Windows, this is a wrapper script, so use CALL, else the script will exit
 CALL %TARGET_FOLDER%\bin\gem install --no-document --silent --force --local cli-gems\*.gem
 ECHO Installing MS redis libs
 vc_redist.x64.exe /install /passive
 ECHO Installing Aspera SDK
-CALL %TARGET_FOLDER%\bin\ascli conf ascp install --sdk-url=file:///sdk.zip
+CALL %TARGET_FOLDER%\bin\ascli conf ascp install --sdk-url=file:///ibm-aspera-transfer-sdk-windows-amd64-1.1.5.zip
 ECHO Aspera Cli is installed at: %TARGET_FOLDER%\bin
 ECHO SET PATH=%%PATH%%;%TARGET_FOLDER%\bin
