55730a1d22242d6ac3f79b4ba38b0e92af682266
Laurent Martin <laurent.martin.l@gmail.com>
2025-07-28 14:46:07 +0200

    preview for text, and add 'file" command

--- lib/aspera/preview/file_types.rb
+++ lib/aspera/preview/file_types.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'aspera/log'
+require 'aspera/assert'
 require 'singleton'
 require 'mime/types'
 
@@ -9,6 +10,7 @@ module Aspera
     # function conversion_type returns one of the types: CONVERSION_TYPES
     class FileTypes
       include Singleton
+
       # values for conversion_type : input format
       CONVERSION_TYPES = %i[image office pdf plaintext video].freeze
 
@@ -22,6 +24,8 @@ module Aspera
         'application/mxf'                    => :video,
         'application/mac-binhex40'           => :office,
         'application/msword'                 => :office,
+        'application/vnd.ms-excel'           => :office,
+        'application/vnd.ms-powerpoint'      => :office,
         'application/rtf'                    => :office,
         'application/x-abiword'              => :office,
         'application/x-mspublisher'          => :office,
@@ -56,17 +60,43 @@ module Aspera
       end
 
       # @param mimetype [String] mime type
-      # @return file type, one of enum CONVERSION_TYPES
+      # @return file type, one of enum CONVERSION_TYPES, or nil if not found
       def mime_to_type(mimetype)
+        Aspera.assert_type(mimetype, String)
         return SUPPORTED_MIME_TYPES[mimetype] if SUPPORTED_MIME_TYPES.key?(mimetype)
-        return :office if mimetype.start_with?('application/vnd.')
+        return :office if mimetype.start_with?('application/vnd.ms-')
+        return :office if mimetype.start_with?('application/vnd.openxmlformats-officedocument')
         return :video if mimetype.start_with?('video/')
         return :image if mimetype.start_with?('image/')
         return nil
       end
 
-      # use mime magic to find mime type based on file content (magic numbers)
-      def file_to_mime(filepath)
+      # @param filepath [String] full path to file
+      # @param mimetype [String] provided by node API
+      # @return file type, one of enum CONVERSION_TYPES
+      # @raise [RuntimeError] if no conversion type found
+      def conversion_type(filepath, mimetype)
+        Log.log.debug{"conversion_type(#{filepath},mime=#{mimetype},magic=#{@use_mimemagic})"}
+        mimetype = nil if mimetype.is_a?(String) && (mimetype == 'application/octet-stream' || mimetype.empty?)
+        # Use mimemagic if available
+        mimetype ||= mime_using_mimemagic(filepath)
+        mimetype ||= mime_using_file(filepath)
+        # from extensions, using local mapping
+        mimetype ||= MIME::Types.of(File.basename(filepath)).first
+        raise "no MIME type found for #{File.basename(filepath)}" if mimetype.nil?
+        conversion_type = mime_to_type(mimetype)
+        raise "no conversion type found for #{File.basename(filepath)}" if conversion_type.nil?
+        Log.log.trace1{"conversion_type(#{File.basename(filepath)}): #{conversion_type.class.name} [#{conversion_type}]"}
+        return conversion_type
+      end
+
+      private
+
+      # Use mime magic to find mime type based on file content (magic numbers)
+      # @param filepath [String] full path to file
+      # @return [String] mime type, or nil if not found
+      def mime_using_mimemagic(filepath)
+        return unless @use_mimemagic
         # moved here, as `mimemagic` can cause installation issues
         require 'mimemagic'
         require 'mimemagic/version'
@@ -83,35 +113,12 @@ module Aspera
         return detected_mime
       end
 
-      # @param filepath [String] full path to file
-      # @param mimetype [String] provided by node API
-      # @return file type, one of enum CONVERSION_TYPES
-      # @raise [RuntimeError] if no conversion type found
-      def conversion_type(filepath, mimetype)
-        Log.log.debug{"conversion_type(#{filepath},m=#{mimetype},t=#{@use_mimemagic})"}
-        # 1- get type from provided mime type, using local mapping
-        conversion_type = mime_to_type(mimetype) if !mimetype.nil?
-        # 2- else, from computed mime type (if available)
-        if conversion_type.nil? && @use_mimemagic
-          detected_mime = file_to_mime(filepath)
-          if !detected_mime.nil?
-            conversion_type = mime_to_type(detected_mime)
-            if !mimetype.nil?
-              if mimetype.eql?(detected_mime)
-                Log.log.debug('matching mime type per magic number')
-              else
-                # NOTE: detected can be nil
-                Log.log.debug{"non matching mime types: node=[#{mimetype}], magic=[#{detected_mime}]"}
-              end
-            end
-          end
-        end
-        # 3- else, from extensions, using local mapping
-        mime_by_ext = MIME::Types.of(File.basename(filepath)).first
-        conversion_type = mime_to_type(mime_by_ext.to_s) if conversion_type.nil? && !mime_by_ext.nil?
-        raise "no conversion type found for #{File.basename(filepath)}" if conversion_type.nil?
-        Log.log.trace1{"conversion_type(#{File.basename(filepath)}): #{conversion_type.class.name} [#{conversion_type}]"}
-        return conversion_type
+      # Use 'file' command to find mime type based on file content (Unix)
+      def mime_using_file(filepath)
+        return Environment.secure_capture(exec: 'file', args: ['--mime-type', '--brief', filepath]).strip
+      rescue => e
+        Log.log.error{"error using 'file' command: #{e.message}"}
+        return nil
       end
     end
   end
