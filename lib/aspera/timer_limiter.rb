25a85640d6f127ef3fc4756cd5ca80fbedb2a56b
Laurent Martin <laurent.martin.l@gmail.com>
2025-07-31 01:08:03 +0200

    fix algorithm

--- lib/aspera/timer_limiter.rb
+++ lib/aspera/timer_limiter.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 module Aspera
-  # used to throttle logs
+  # trigger returns true only if the delay has passed since the last trigger
   class TimerLimiter
     # @param delay in seconds (float)
     def initialize(delay)
       @delay = delay
-      @last_time = nil
+      @last_trigger_time = nil
       @count = 0
     end
 
+    # Check if the trigger condition is met
+    # @return [Boolean] true if the trigger condition is met, false otherwise
     def trigger?
-      old_time = @last_time
-      @last_time = Time.now.to_f
+      current_time = Time.now.to_f
       @count += 1
-      if old_time.nil? || ((@last_time - old_time) > @delay)
+      if @last_trigger_time.nil? || ((current_time - @last_trigger_time) > @delay)
+        @last_trigger_time = current_time
         @count = 0
         return true
       end
