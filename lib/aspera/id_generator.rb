18629a40f11d73e7260d4904173923945a80b9f5
Laurent Martin <laurent.martin.l@gmail.com>
2025-09-01 13:04:37 +0200

    use environment funcs

--- lib/aspera/id_generator.rb
+++ lib/aspera/id_generator.rb
@@ -1,27 +1,27 @@
 # frozen_string_literal: true
 
 require 'aspera/assert'
+require 'aspera/environment'
 require 'uri'
 
 module Aspera
   class IdGenerator
-    ID_SEPARATOR = '_'
-    WINDOWS_PROTECTED_CHAR = %r{[/:"<>\\*?]}
-    PROTECTED_CHAR_REPLACE = '_'
-    private_constant :ID_SEPARATOR, :PROTECTED_CHAR_REPLACE, :WINDOWS_PROTECTED_CHAR
     class << self
+      # Generate an ID from a list of object IDs
+      # The generated ID is safe as file name
+      # @param object_id [Array<String>, String] the object IDs
+      # @return [String] the generated ID
       def from_list(object_id)
+        safe_char = Environment.instance.safe_filename_character
         if object_id.is_a?(Array)
           # compact: remove nils
           object_id = object_id.compact.map do |i|
             i.is_a?(String) && i.start_with?('https://') ? URI.parse(i).host : i.to_s
-          end.join(ID_SEPARATOR)
+          end.join(safe_char)
         end
         Aspera.assert_type(object_id, String)
-        return object_id
-            .gsub(WINDOWS_PROTECTED_CHAR, PROTECTED_CHAR_REPLACE) # remove windows forbidden chars
-            .gsub('.', PROTECTED_CHAR_REPLACE) # keep dot for extension only (nicer)
-            .downcase
+        # keep dot for extension only (nicer)
+        return Environment.instance.sanitized_filename(object_id.gsub('.', safe_char)).downcase
       end
     end
   end
