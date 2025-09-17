34be0df84dc37bfb786f1e42a2e02135d90085c5
Laurent Martin <laurent.martin.l@gmail.com>
2025-09-17 12:49:33 +0200

    fixes in transfer spec documentation and generation.

--- lib/aspera/transfer/spec_doc.rb
+++ lib/aspera/transfer/spec_doc.rb
@@ -12,11 +12,10 @@ module Aspera
           agent_sym.to_sym.eql?(:direct) ? :a : agent_sym.to_s[0].to_sym
         end
 
-        # @param formatter [Cli::Formatter] formatter to use, methods: special_format, check_row
-        # @param cli [Boolean] whether to include CLI options
-        # @param color [Boolean] whether to include color formatting
-        # @return a table suitable to display in manual
-        def man_table(formatter, cli: true, color: true)
+        # @param formatter      [Cli::Formatter] Formatter to use, methods: special_format, check_row
+        # @param include_option [Boolean] true : include CLI options
+        # @return [Array] a table suitable to display in manual
+        def man_table(formatter, include_option: false)
           col_local = agent_to_short(:direct)
           Spec::SCHEMA['properties'].filter_map do |name, properties|
             # manual table
@@ -25,17 +24,24 @@ module Aspera
               type:        properties['type'],
               description: []
             }
-            # replace "back solidus" HTML entity with its text value and split lines
-            columns[:description].concat(properties['description'].gsub('&bsol;', '\\').split("\n")) if properties.key?('description')
+            # replace "back solidus" HTML entity with its text value
+            #
+            # split lines
+            columns[:description] =
+              properties['description']
+                .gsub('&bsol;', '\\')
+                .gsub(/`([a-z0-9_.+-]+)`/){formatter.keyword_highlight(Regexp.last_match(1))}
+                .split("\n") if properties.key?('description')
             columns[:description].unshift("DEPRECATED: #{properties['x-deprecation']}") if properties.key?('x-deprecation')
             # add flags for supported agents in doc
             AGENT_LIST.each do |agent_info|
-              columns[agent_info.last] = Cli::Formatter.tick(properties['x-agents'].nil? || properties['x-agents'].include?(agent_info.first.to_s), color: color)
+              columns[agent_info.last] = formatter.tick(properties['x-agents'].nil? || properties['x-agents'].include?(agent_info.first.to_s))
             end
-            columns[col_local] = Cli::Formatter.tick(true, color: color) if properties['x-cli-option']
+            columns[col_local] = formatter.tick(true) if properties['x-cli-option']
             # only keep lines that are usable in supported agents
             next false if AGENT_LIST.map(&:last).inject(true){ |memory, agent_short_sym| memory && columns[agent_short_sym].empty?}
-            columns[:description].push("Allowed values: #{properties['enum'].join(', ')}") if properties.key?('enum')
+            columns[:description].push("Allowed values: #{properties['enum'].map{ |v| formatter.keyword_highlight(v)}.join(', ')}") if properties.key?('enum')
+            envvar = ''
             cli_option =
               if properties['x-cli-switch']
                 properties['x-cli-option']
@@ -47,8 +53,11 @@ module Aspera
                 sep = properties['x-cli-option'].start_with?('--') ? '=' : ' '
                 "#{properties['x-cli-option']}#{sep}#{conversion_tag}#{arg_type}"
               end
-            cli_option = 'env:' + properties['x-cli-envvar'] if properties.key?('x-cli-envvar')
-            columns[:description].push("(#{cli_option})") if cli && !cli_option.to_s.empty?
+            if properties.key?('x-cli-envvar')
+              cli_option = properties['x-cli-envvar']
+              envvar = 'env:'
+            end
+            columns[:description].push("(#{envvar}#{formatter.keyword_highlight(cli_option)})") if include_option && !cli_option.to_s.empty?
             formatter.check_row(columns)
           end.sort_by{ |i| i[:name]}
         end
