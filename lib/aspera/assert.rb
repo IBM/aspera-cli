37df2408103011a16e451b2760e6f232f5e2c8f2
Laurent Martin <laurent.martin.l@gmail.com>
2025-09-17 16:29:26 +0200

    assert with possibility to only log

--- lib/aspera/assert.rb
+++ lib/aspera/assert.rb
@@ -13,11 +13,22 @@ module Aspera
   class AssertError < Error
   end
   class << self
+    # Replace `raise`, allows sending exception, or just error log, when type is `:error`
+    # @param type [Exception,Symbol] Send to log if symbol, else raise exception
+    # @param message [String] Message for error.
+    def report_error(type, message)
+      if type.is_a?(Symbol)
+        Log.log.send(type, message)
+      else
+        raise type, message
+      end
+    end
+
     # Assert that a condition is true, else raise exception
     # @param assertion [Bool]       Must be true
-    # @param info      [String,nil] Fixed message in case assert fails
-    # @param type      [Exception]  Exception to raise
-    # @param block     [Proc]       Like info, a string that desribes the problem
+    # @param info      [String,nil] Fixed message in case assert fails, else use `block`
+    # @param type      [Exception,Symbol] Exception to raise, or Symbol for Log.log
+    # @param block     [Proc]       Produces a string that desribes the problem for complex messages
     # The block is executed in the context of the Aspera module
     def assert(assertion, info = nil, type: AssertError)
       raise InternalError, 'bad assert: both info and block given' unless info.nil? || !block_given?
@@ -26,20 +37,23 @@ module Aspera
       info = yield if block_given?
       message = "#{message}: #{info}" if info
       message = "#{message}: #{caller.find{ |call| !call.start_with?(__FILE__)}}"
-      raise type, message
+      report_error(type, message)
     end
 
     # Assert that value has the given type
-    # @param value [Object] the value to check
-    # @param classes [Class, Array] the expected type(s)
+    # @param value   [Object]       The value to check
+    # @param classes [Class, Array] The expected type(s)
+    # @param type    [Exception,Symbol] Exception to raise, or Symbol for Log.log
+    # @param block   [Proc]         Additional description in front of message
     def assert_type(value, *classes, type: AssertError)
       assert(classes.any?{ |k| value.is_a?(k)}, type: type){"#{"#{yield}: " if block_given?}expecting #{classes.join(', ')}, but have #{value.inspect}"}
     end
 
     # Assert that value is one of the given values
-    # @param value value to check
-    # @param values accepted values
-    # @param type exception in case of no match
+    # @param value  [any]   value to check
+    # @param values [Array] accepted values
+    # @param type   [Exception,Symbol] Exception to raise, or Symbol for Log.log
+    # @param block  [Proc]         Additional description in front of message
     def assert_values(value, values, type: AssertError)
       assert(values.include?(value), type: type) do
         val_list = values.inspect
@@ -48,17 +62,17 @@ module Aspera
       end
     end
 
+    # The value is not one of the expected values
+    # @param value  [any]              The wrong value
+    # @param type   [Exception,Symbol] Exception to raise, or Symbol for Log.log
+    # @param block  [Proc]             Additional description in front of message
+    def error_unexpected_value(value, type: InternalError)
+      report_error(type, "#{"#{yield}: " if block_given?}unexpected value: #{value.inspect}")
+    end
+
     # The line with this shall never be reached
     def error_unreachable_line
       raise InternalError, "unreachable line reached: #{caller(2..2).first}"
-    end
-
-    # The value is not one of the expected values
-    # @param value the wrong value
-    # @param type exception to raise
-    # @param block additional description in front
-    def error_unexpected_value(value, type: InternalError)
-      raise type, "#{"#{yield}: " if block_given?}unexpected value: #{value.inspect}"
     end
 
     # Not implemented error
