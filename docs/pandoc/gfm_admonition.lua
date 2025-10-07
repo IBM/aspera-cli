efd65fdbcaf5e7eaec2a9419613bbe58e569a9f3
Laurent Martin <laurent.martin.l@gmail.com>
2025-10-07 13:54:43 +0200

    re-reorganize

--- docs/pandoc/gfm_admonition.lua
+++ docs/pandoc/gfm_admonition.lua
@@ -2,60 +2,87 @@
 -- corresponding HTML or LaTeX representations with icons, colors, and titles.
 -- Uses predefined visual styles from `box_styles` for consistent formatting.
 -- luacheck: globals FORMAT pandoc
+-- https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#alerts
+-- https://tug.ctan.org/info/symbols/comprehensive/symbols-a4.pdf
 local box_styles = {
   note = {
-    colback      = "blue!5",
-    coltitle     = "white",
-    colbacktitle = "blue!75!black",
-    htmlclass    = "admonition note",
-    latexicon    = "InfoCircle",
-    htmlicon     = "ℹ️ ",
-  },
-  caution = {
-    colback      = "yellow!5",
-    coltitle     = "black",
-    colbacktitle = "yellow!70!black",
-    htmlclass    = "admonition caution",
-    latexicon    = "ExclamationTriangle",
-    htmlicon     = "⚠️ ",
-  },
-  warning = {
-    colback      = "red!5",
-    coltitle     = "white",
-    colbacktitle = "red!75!black",
-    htmlclass    = "admonition warning",
-    latexicon    = "ExclamationTriangle",
-    htmlicon     = "⚠️ ",
-  },
-  important = {
-    colback      = "orange!5",
-    coltitle     = "white",
-    colbacktitle = "orange!70!black",
-    htmlclass    = "admonition important",
-    latexicon    = "ExclamationTriangle",
-    htmlicon     = "❗ ",
-  },
-  tip = {
-    colback      = "green!5",
-    coltitle     = "white",
-    colbacktitle = "green!60!black",
-    htmlclass    = "admonition tip",
-    latexicon    = "Lightbulb",
-    htmlicon     = "💡 ",
+    -- gfm : blue circled i
+    html  = {
+      icon = "ℹ️",
+    },
+    latex = {
+      icon      = "InfoCircle",
+      back      = "blue!5",
+      title     = "white",
+      backtitle = "blue!75!black",
+    },
   },
   info = {
-    colback      = "gray!5",
-    coltitle     = "black",
-    colbacktitle = "gray!60!black",
-    htmlclass    = "admonition info",
-    latexicon    = "InfoCircle",
-    htmlicon     = "ℹ️ ",
+    -- gfm : ? not exists
+    html  = {
+      icon = "ℹ️",
+    },
+    latex = {
+      icon      = "InfoCircle",
+      back      = "gray!5",
+      title     = "black",
+      backtitle = "gray!60!black",
+    },
+  },
+  tip = {
+    -- gfm : green lightbulb
+    html  = {
+      icon = "💡",
+    },
+    latex = {
+      icon      = "Lightbulb",
+      back      = "green!5",
+      title     = "white",
+      backtitle = "green!60!black",
+    },
+  },
+  important = {
+    -- gfm : purple speech bubble exclamation
+    html  = {
+      icon = "❗",
+    },
+    latex = {
+      icon      = "ExclamationTriangle",
+      back      = "orange!5",
+      title     = "white",
+      backtitle = "orange!70!black",
+    },
+  },
+  warning = {
+    -- gfm : yellow triangle exclamation
+    html  = {
+      icon = "⚠️",
+    },
+    latex = {
+      icon      = "ExclamationTriangle",
+      back      = "red!5",
+      title     = "white",
+      backtitle = "red!75!black",
+    },
+  },
+  caution = {
+    -- gfm : red circled exclamation mark
+    html  = {
+      icon = "⚡",
+    },
+    latex = {
+      icon      = "ExclamationTriangle",
+      back      = "yellow!5",
+      title     = "black",
+      backtitle = "yellow!70!black",
+    },
   },
 }
 
 function Div(el)
   if #el.classes == 0 then return nil end
-  local style = box_styles[el.classes[1]]
+  local admon_type = el.classes[1]
+  local style = box_styles[admon_type]
   if not style then return nil end
   if not (el.content[1] and el.content[1].t == "Div" and el.content[1].classes:includes("title")) then return el end
   local title_div_content = el.content[1].content
@@ -66,19 +93,21 @@ function Div(el)
     table.insert(body_parts, block)
   end
   if FORMAT:match("latex") then
+    style = style.latex
     local opts = string.format(
       "enhanced, breakable, colback=%s, colframe=%s, coltitle=%s, colbacktitle=%s, title={%s}",
-      style.colback, style.colbacktitle, style.coltitle, style.colbacktitle, "\\fa" .. style.latexicon .. "\\ " .. title
+      style.back, style.backtitle, style.title, style.backtitle, "\\fa" .. style.icon .. "\\ " .. title
     )
     local body = pandoc.write(pandoc.Pandoc(body_parts), "latex")
     return pandoc.RawBlock("latex",
       "\\begin{tcolorbox}[" .. opts .. "]\n" ..
       body .. "\n\\end{tcolorbox}")
   elseif FORMAT:match("html") then
+    style = style.html
     local body = pandoc.write(pandoc.Pandoc(body_parts), "html")
     local html = string.format(
-      '<div class="%s">\n<div class="admonition-title">%s %s</div>\n<div class="admonition-body">%s</div>\n</div>',
-      style.htmlclass, style.htmlicon, title, body
+      '<div class="admonition %s">\n<div class="admonition-title">%s %s</div>\n<div class="admonition-body">%s</div>\n</div>',
+      admon_type, style.icon, title, body
     )
     return pandoc.RawBlock("html", html)
   end
