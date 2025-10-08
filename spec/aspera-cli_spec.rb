38ce6b2b36831a5d0a834c012a9092ed3b9bd710
Laurent Martin <laurent.martin.l@gmail.com>
2025-10-08 11:00:53 +0200

    single spec for coverage

--- spec/aspera-cli_spec.rb
+++ spec/aspera-cli_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-
-# top folder of project
-project_top_folder = File.dirname(File.realpath(__FILE__), 2)
-
 require 'aspera/log'
 # Aspera::Log.instance.level = :debug
 # Aspera::Log.instance.logger_type = :stderr
@@ -14,6 +10,10 @@ require 'aspera/cli/main'
 require 'aspera/ascmd'
 require 'aspera/assert'
 require 'aspera/ssh'
+require 'aspera/rest'
+require 'aspera/uri_reader'
+require 'aspera/environment'
+require 'aspera/ascp/management'
 require 'uri'
 require 'openssl'
 
@@ -43,6 +43,8 @@ if defined?(JRUBY_VERSION)
   # :nocov:
 end
 demo_executor = Aspera::Ssh.new(ssh_url.host, RSpec.configuration.username, **ssh_options)
+# top folder of project
+project_top_folder = File.dirname(File.realpath(__FILE__), 2)
 
 # to use a local executor, set PATH_FOLDER_MAIN to the pseudo docroot (local) folder
 # PATH_FOLDER_MAIN='/pseudo/docroot'
@@ -232,5 +234,91 @@ RSpec.describe(Aspera::Ssh) do
     expect do
       demo_executor.execute('foo', exception: true)
     end.to(raise_error(Aspera::Ssh::Error, /Command not accepted: foo/))
+  end
+end
+
+RSpec.describe(Aspera::AssertError) do
+  it 'works for list' do
+    Aspera.assert_values(:bad, [:good])
+    raise 'Shall not reach here'
+  rescue Aspera::AssertError => e
+    expect(e.message).to(start_with('assertion failed: expecting one of [:good], but have :bad'))
+  end
+end
+
+RSpec.describe(Aspera::UriReader) do
+  it 'fails on bad uri' do
+    Aspera::UriReader.read('unknown:///foo.bar')
+    raise 'Shall not reach here'
+  rescue Aspera::InternalError => e
+    expect(e.message).to(include('unexpected value: "unknown"'))
+  end
+  it 'fails on bad file uri' do
+    Aspera::UriReader.read_as_file('file:foo.bar')
+    raise 'Shall not reach here'
+  rescue RuntimeError => e
+    expect(e.message).to(start_with('use format: file:///'))
+  end
+end
+
+RSpec.describe(Aspera::Environment) do
+  it 'works for OSes' do
+    RbConfig::CONFIG['host_os'] = 'mswin'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.os).to(eq(Aspera::Environment::OS_WINDOWS))
+    expect(Aspera::Environment.instance.executable_extension).to(eq('exe'))
+    RbConfig::CONFIG['host_os'] = 'darwin'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.os).to(eq(Aspera::Environment::OS_MACOS))
+    RbConfig::CONFIG['host_os'] = 'linux'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.os).to(eq(Aspera::Environment::OS_LINUX))
+    expect(Aspera::Environment.instance.executable_extension).to(eq(nil))
+    RbConfig::CONFIG['host_os'] = 'aix'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.os).to(eq(Aspera::Environment::OS_AIX))
+  end
+  it 'works for CPUs' do
+    RbConfig::CONFIG['host_cpu'] = 'x86_64'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.cpu).to(eq(Aspera::Environment::CPU_X86_64))
+    RbConfig::CONFIG['host_cpu'] = 'powerpc'
+    RbConfig::CONFIG['host_os'] = 'linux'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.cpu).to(eq(Aspera::Environment::CPU_PPC64LE))
+    RbConfig::CONFIG['host_os'] = 'aix'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.cpu).to(eq(Aspera::Environment::CPU_PPC64))
+    RbConfig::CONFIG['host_cpu'] = 's390'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.cpu).to(eq(Aspera::Environment::CPU_S390))
+    RbConfig::CONFIG['host_cpu'] = 'arm'
+    Aspera::Environment.instance.initialize_fields
+    expect(Aspera::Environment.instance.cpu).to(eq(Aspera::Environment::CPU_ARM64))
+  end
+  it 'works for event' do
+    event = {
+      'Bytescont'         => '1',
+      'Elapsedusec'       => '10',
+      'Encryption'        => 'Yes',
+      'ExtraCreatePolicy' => 'none'
+    }
+    newevent = Aspera::Ascp::Management.enhanced_event_format(event)
+    expect(newevent).to(eq({
+      'bytes_cont'          => 1,
+      'elapsed_usec'        => 10,
+      'encryption'          => true,
+      'extra_create_policy' => 'none'
+    }))
+  end
+end
+
+RSpec.describe(Aspera::Rest) do
+  it 'build URI' do
+    expect(Aspera::Rest.build_uri('https://locahost', 'q=e&p=1').to_s).to(eq('https://locahost?q=e&p=1'))
+  end
+  it 'parses php query' do
+    expect(Aspera::Rest.query_to_h('q[]=1&q[]=2')).to(eq({'q'=>%w[1 2]}))
+    expect(Aspera::Rest.query_to_h('q=1&q=2')).to(eq({'q'=>%w[1 2]}))
   end
 end
