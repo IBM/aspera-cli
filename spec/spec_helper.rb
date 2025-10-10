59ee4f5ed9a0f622b751e9ba9272ecea05478f10
Laurent Martin <laurent.martin.l@gmail.com>
2025-10-11 01:09:51 +0200

    simplification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
-# Parameters are provided with env var RSPEC_CONFIG set to a json with url, username, password
+# parameters in `server_user` of `ASPERA_CLI_TEST_CONF_FILE`
+# or direxctly in JSON: RSPEC_CONFIG
 
 require 'bundler/setup'
-require 'json'
+require 'yaml'
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
   config.expect_with(:rspec) do |c|
     c.syntax = :expect
   end
-  raise 'Missing env var: RSPEC_CONFIG' unless ENV.key?('RSPEC_CONFIG')
-  params = JSON.parse(ENV['RSPEC_CONFIG'])
+  params = if ENV.key?('RSPEC_CONFIG')
+    JSON.parse(ENV['RSPEC_CONFIG'])
+  else
+    raise 'Missing env var: ASPERA_CLI_TEST_CONF_FILE' unless ENV.key?('ASPERA_CLI_TEST_CONF_FILE')
+    YAML.load_file(ENV['ASPERA_CLI_TEST_CONF_FILE'])['server_user']
+  end
   %i[url username password].each do |p|
     param = params[p.to_s]
     raise "Missing parameter: #{p}" if param.nil?
