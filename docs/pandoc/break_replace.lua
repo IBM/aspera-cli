b577ad6e6b26e4f8700fa20496c0157b99c4e9d3
Laurent Martin <laurent.martin.l@gmail.com>
2025-10-20 21:20:39 +0200

    indent

--- docs/pandoc/break_replace.lua
+++ docs/pandoc/break_replace.lua
@@ -5,19 +5,19 @@
 -- https://pandoc.org/lua-filters.html#pandoc.Table
 function Table(tbl)
     for _, body in ipairs(tbl.bodies) do
-    for _, row in ipairs(body.body) do
-        for _, cell in ipairs(row.cells) do
+        for _, row in ipairs(body.body) do
+            for _, cell in ipairs(row.cells) do
                 for _, block in ipairs(cell.content) do
-                if block.t == "Plain" or block.t == "Para" then
-                    for i, el in ipairs(block.content) do
-                        if el.t == "RawInline" and el.format == "html" and el.text == "<br/>" then
-                            block.content[i] = pandoc.LineBreak()
+                    if block.t == "Plain" or block.t == "Para" then
+                        for i, el in ipairs(block.content) do
+                            if el.t == "RawInline" and el.format == "html" and el.text == "<br/>" then
+                                block.content[i] = pandoc.LineBreak()
+                            end
                         end
                     end
                 end
             end
         end
-    end
     end
     return tbl
 end
