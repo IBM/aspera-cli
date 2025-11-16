315f041dfc3d5ec74bba9f2e85c748fa0ca96f7f
Laurent Martin <laurent.martin.l@gmail.com>
2025-11-16 16:18:48 +0100

    update

--- windows/build.rb
+++ windows/build.rb
@@ -6,32 +6,36 @@ require 'erb'
 require 'aspera/rest'
 require 'aspera/colors'
 require 'aspera/environment'
+require 'aspera/cli/info'
+require 'aspera/cli/version'
 require 'fileutils'
 require 'aspera/cli/transfer_progress'
 require 'aspera/ascp/installation'
 Aspera::RestParameters.instance.progress_bar = Aspera::Cli::TransferProgress.new
 
-GEM_NAME = 'aspera-cli'
-GEM_VERSION = '4.24.2'
-RUBY_INSTALLER_BASE_URL = 'https://github.com/oneclick/rubyinstaller2/releases/download'
-RUBY_INST_VERSION = '3.4.7-1'
+Aspera::Cli::Info::GEM_NAME = 'aspera-cli'
+GEM_VERSION = ARGV.first || Aspera::Cli::VERSION
+INSTALL_RUBY_BASE_URL = 'https://github.com/oneclick/rubyinstaller2/releases/download'
+INSTALL_RUBY_VERSION = '3.4.7-1'
 MS_VC_BASE_URL = 'https://aka.ms/vc14'
 TARGET_FOLDER_NAME = "aspera-cli-#{GEM_VERSION}-windows-amd64-installer"
 TEMP_FOLDER = File.join(File.dirname(__FILE__, 2), 'tmp')
 
-def zip_directory(source_dir, zip_path)
-  source_dir = File.expand_path(source_dir)
-  Aspera.assert(Dir.exist?(source_dir)){"Source directory not found: #{source_dir}"}
-  File.delete(zip_path) if File.exist?(zip_path)
-  prefix_length = (File.dirname(source_dir).length + 1)
-  Zip::File.open(zip_path, create: true) do |zipfile|
-    Dir.glob(File.join(source_dir, '**', '*')).each do |source_path|
+# ZIP the source folder to zip file
+# does not include folder name
+def zip_directory(folder, zip)
+  folder = File.expand_path(folder)
+  Aspera.assert(Dir.exist?(folder)){"Source directory not found: #{folder}"}
+  File.delete(zip) if File.exist?(zip)
+  prefix_length = (folder.length + 1)
+  Zip::File.open(zip, create: true) do |zipfile|
+    Dir.glob(File.join(folder, '**', '*')).each do |source_path|
       zipfile.add(source_path[prefix_length..-1], source_path)
     end
   end
 end
 
-puts("Generating Windows package for #{GEM_NAME} v#{GEM_VERSION}")
+puts("Generating Windows package for #{Aspera::Cli::Info::GEM_NAME} v#{GEM_VERSION}")
 build_folder = File.join(TEMP_FOLDER, TARGET_FOLDER_NAME)
 FileUtils.rm_rf(build_folder)
 FileUtils.mkdir_p(build_folder)
@@ -46,14 +50,14 @@ Aspera::Rest.new(base_url: sdk_base, redirect_max: 5).read(sdk_file, save_to_fil
 puts('Getting gems'.blue)
 gem_archive_dir = 'rbgems'
 tmp_install_ruby = File.join(build_folder, 'tmpruby')
-Aspera::Environment.secure_execute(exec: 'gem', args: ['install', "#{GEM_NAME}:#{GEM_VERSION}", '--no-document', '--install-dir', tmp_install_ruby])
+Aspera::Environment.secure_execute(exec: 'gem', args: ['install', "#{Aspera::Cli::Info::GEM_NAME}:#{GEM_VERSION}", '--no-document', '--install-dir', tmp_install_ruby])
 File.rename(File.join(tmp_install_ruby, 'cache'), File.join(build_folder, gem_archive_dir))
 FileUtils.rm_rf(tmp_install_ruby)
 
 puts('Getting Ruby'.blue)
-ruby_installer_exe = "rubyinstaller-devkit-#{RUBY_INST_VERSION}-x64.exe"
-source_file = "RubyInstaller-#{RUBY_INST_VERSION}/#{ruby_installer_exe}"
-Aspera::Rest.new(base_url: RUBY_INSTALLER_BASE_URL, redirect_max: 5).read(source_file, save_to_file: File.join(build_folder, ruby_installer_exe))
+ruby_installer_exe = "rubyinstaller-devkit-#{INSTALL_RUBY_VERSION}-x64.exe"
+source_file = "RubyInstaller-#{INSTALL_RUBY_VERSION}/#{ruby_installer_exe}"
+Aspera::Rest.new(base_url: INSTALL_RUBY_BASE_URL, redirect_max: 5).read(source_file, save_to_file: File.join(build_folder, ruby_installer_exe))
 
 puts('Getting VC++ Redistributable'.blue)
 vc_redist_exe = 'vc_redist.x64.exe'
---
+++ windows/install.cmd
@@ -0,0 +1,23 @@
+@echo off
+REM ====================================================
+REM Installer bootstrap for Windows
+REM Launches PowerShell script with bypass
+REM ====================================================
+
+REM Get the directory of this script
+set "SCRIPT_DIR=%~dp0"
+
+REM Launch PowerShell installer
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%do-install.ps1" %*
+
+REM Check exit code
+if %ERRORLEVEL% neq 0 (
+    echo.
+    echo [ERROR] Installation failed with code %ERRORLEVEL%.
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+echo Installation completed successfully.
+pause
