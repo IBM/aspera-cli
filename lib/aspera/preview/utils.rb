8c06c4f1a4611e844354f66983ea34133d543c8f
Laurent Martin <laurent.martin.l@gmail.com>
2026-09-22 18:08:52 +0200

    improve message
    Signed-off-by: Laurent Martin <laurent.martin.l@gmail.com>

--- lib/aspera/preview/utils.rb
+++ lib/aspera/preview/utils.rb
@@ -35,8 +35,10 @@ module Aspera
         def available_h264_encoder
           return @available_h264_encoder if defined?(@available_h264_encoder)
           stdout, = execute(:ffmpeg, '-encoders', mode: :capture, exception: false)
-          @available_h264_encoder = H264_ENCODER_PREFERENCE.find { |enc| stdout.include?(enc) } ||
-            raise("No supported H.264 encoder found in ffmpeg. Available: #{stdout.lines.grep(/264/).map(&:strip).join(', ')}")
+          available = stdout.lines.grep(/h264/i).map { |l| l.split[1] }
+          @available_h264_encoder = H264_ENCODER_PREFERENCE.find { |enc| available.include?(enc) }
+          Aspera.assert(@available_h264_encoder) { "No supported H.264 encoder found in ffmpeg. Available: #{available.join(', ')}" }
+          @available_h264_encoder
         end
 
         # Check that external tools can be executed.
